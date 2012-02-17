c9de107c0209000f6923842638b031e5853fff11
Dean Harding <dean@codeka.com.au>
2012-02-17 09:55:48 +0000

    Deleted the AppEngine project, we're now 100% Python.

--- WarWorlds-AppEngine/shared/au/com/codeka/warworlds/shared/DevicesNotificationsResource.java
+++
@@ -1,8 +0,0 @@
-package au.com.codeka.warworlds.shared;
-
-import org.restlet.resource.Put;
-
-public interface DevicesNotificationsResource {
-	@Put
-	public void send(Notification n);
-}
--- WarWorlds-AppEngine/shared/au/com/codeka/warworlds/shared/constants/SectorConstants.java
+++
@@ -1,9 +0,0 @@
-package au.com.codeka.warworlds.shared.constants;
-
-public class SectorConstants {
-    public static int Width = 1024;
-    public static int Height = 1024;
-
-    public static int MinStars = 10;
-    public static int MaxStars = 15;
-}
