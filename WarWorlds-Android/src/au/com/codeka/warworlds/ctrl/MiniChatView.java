1bbc93bf7c271c43a942b9c369b056947c2a4aa0
Dean Harding <dean@codeka.com.au>
2012-03-24 10:53:28 +0000

    Added the 'chat' button, though it's not hooked up.

--- WarWorlds-Android/src/au/com/codeka/warworlds/ctrl/MiniChatView.java
+++ WarWorlds-Android/src/au/com/codeka/warworlds/ctrl/MiniChatView.java
@@ -1,12 +1,17 @@
 package au.com.codeka.warworlds.ctrl;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import au.com.codeka.warworlds.R;
 import au.com.codeka.warworlds.game.ChatManager;
 import au.com.codeka.warworlds.model.ChatMessage;
 
@@ -14,7 +19,7 @@ import au.com.codeka.warworlds.model.ChatMessage;
  * This control displays the mini chat window, which displays recent chat
  * messages on each screen.
  */
-public class MiniChatView extends LinearLayout {
+public class MiniChatView extends RelativeLayout {
     private Context mContext;
 
     private ScrollView mScrollView;
@@ -25,16 +30,32 @@ public class MiniChatView extends LinearLayout {
         super(context, attrs);
         mContext = context;
 
+        int id = 1;
         this.setBackgroundColor(Color.argb(0xaa, 0, 0, 0));
 
-        LayoutParams lp = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0);
+        Resources r = getResources();
+        int size = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, r.getDisplayMetrics());
+        LayoutParams lp = new LayoutParams(size, size);
+
+        Button btn = new Button(mContext);
+        btn.setLayoutParams(lp);
+        btn.setBackgroundResource(R.drawable.button_bg);
+        btn.setId(id++);
+        btn.setCompoundDrawablesWithIntrinsicBounds(R.drawable.chat_icon, 0, 0, 0);
+        addView(btn);
+
+        lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        lp.addRule(RelativeLayout.RIGHT_OF, btn.getId());
         mScrollView = new ScrollView(mContext);
         mScrollView.setLayoutParams(lp);
-        this.addView(mScrollView);
+        mScrollView.setId(id++);
+        addView(mScrollView);
 
+        lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         mMsgsContainer = new LinearLayout(mContext);
         mMsgsContainer.setOrientation(LinearLayout.VERTICAL);
         mMsgsContainer.setLayoutParams(lp);
+        mMsgsContainer.setId(id++);
         mScrollView.addView(mMsgsContainer);
 
         mMessageAddedListener = new MessageAddedListener();
