b3d61de33de32280eeb106abb0bfdb637616a14a
Dean Harding <dean@codeka.com.au>
2012-07-09 11:03:14 +0000

    New name generator algorithm.

--- WarWorlds-Server/model/namegen.py
+++ WarWorlds-Server/model/namegen.py
@@ -2,152 +2,101 @@
 
 import os
 import random
+import codecs
 import logging
 
 
-_generators = []
+_vocabularies = []
 
 
-def generate(numNames, minNameSyllables = 2, maxNameSyllables = 4):
-  """Generates numNames names from the vocabularies defined in the vocabulary directory."""
-
-  global _generators
-  _ensureGeneratorsLoaded()
+def generate(numNames):
+  global _vocabularies
+  _ensureVocabulariesLoaded()
 
   names = []
   for _ in range(numNames):
-    genIndex = random.randint(0, len(_generators)-1)
-    numSyllables = random.randint(minNameSyllables, maxNameSyllables)
-    names.append(_generators[genIndex].compose(numSyllables))
+    vocabIndex = random.randint(0, len(_vocabularies)-1)
+    vocabulary = _vocabularies[vocabIndex]
+    names.append(_generateName(vocabulary))
   return names
 
-def _ensureGeneratorsLoaded():
-  """Ensures that we've loaded the NameGenerators. We only need to do it once."""
 
-  global _generators
-  if len(_generators) > 0:
+def _ensureVocabulariesLoaded():
+  """Ensures that we've loaded the vocabulary files. We only need to do it once."""
+  global _vocabularies
+  if len(_vocabularies) > 0:
     return
 
-  generators = []
+  vocabularies = []
   rootFolder = os.path.dirname(__file__)+"/vocabulary"
   for fileName in os.listdir(rootFolder):
     if fileName.endswith(".txt"):
       path = rootFolder+"/"+fileName
       logging.info("Loading vocabulary file: "+path)
-      generator = NameGenerator()
-      with open(path, "r") as inf:
-        generator.parse(inf)
-      generators.append(generator)
-  _generators = generators
+      vocabularies.append(_parseVocabulary(path))
+  _vocabularies = vocabularies
 
 
-class NameGenerator:
-  def __init__(self):
-    self.prefixes = []
-    self.suffixes = []
-    self.middles = []
+def _parseVocabulary(file_name):
+  """This class parses a vocabulary file.
 
-  def parse(self, inf):
-    """Parses a vocabulary file and populates our internal data so that we can generate names."""
+  The vocabulary file is really just a list of words in a certain "vocabulary" (e.g. Viking names,
+  English names, etc). We parse the words from that file, and for each pair of adjacent letters
+  we determine which letters are most comment next to each other.
+  """
+  letter_frequencies = {}
 
-    self.prefixes = []
-    self.suffixes = []
-    self.middles = []
+  def addLetter(last_letters, letter):
+    if last_letters not in letter_frequencies:
+      frequency = {}
+      letter_frequencies[last_letters] = frequency
+    else:
+      frequency = letter_frequencies[last_letters]
+    if letter not in frequency:
+      frequency[letter] = 1
+    else:
+      frequency[letter] += 1
 
+  with codecs.open(file_name, 'r', encoding='utf8') as inf:
     for line in inf:
-      # remove comments (everything after a '#'
-      line = line.split('#', 2)[0]
-      line = line.strip()
+      for word in line.split():
+        last_letters = u'  '
+        for letter in word:
+          addLetter(last_letters, letter)
+          last_letters = last_letters[-1]+letter
+        addLetter(word[-2:], u' ')
 
-      # ignore blank lines
-      if line == "":
-        continue
+  return letter_frequencies
 
-      isPrefix = False
-      isSuffix = False
-      if line[0] == '+':
-        isPrefix = True
-        line = line[1:].strip()
-      elif line[0] == '-':
-        isSuffix = True
-        line = line[1:].strip()
 
-      tokens = line.split()
-      syllable = _Syllable()
-      syllable.syllable = tokens[0].lower()
-      for token in tokens[1:]:
-        if token == '+v':
-          syllable.requiresNextVowel = True
-        elif token == '-v':
-          syllable.requiresPreviousVowel = True
-        elif token == '+c':
-          syllable.requiresNextConsonant = True
-        elif token == '-c':
-          syllable.requiresPreviousConsonant = True
+def _generateName(vocabulary):
+  """Generates a single name from the given vocabulary data."""
+  def getLetter(last_letter):
+    if last_letter not in vocabulary:
+      raise ValueError(last_letter)
+    frequencies = vocabulary[last_letter]
+    max_frequency = 0
+    for _,count in frequencies.iteritems():
+      max_frequency += count
+    index = random.randint(0, max_frequency)
+    for letter,count in frequencies.iteritems():
+      index -= count
+      if index <= 0:
+        return letter
+    return ' '
 
-      syllable.startsWithVowel = self._isVowel(syllable.syllable[0])
-      syllable.endsWithVowel = self._isVowel(syllable.syllable[-1])
+  word = getLetter(u'  ')
+  word += getLetter(u' '+word[-1])
+  while word[-1] != u' ' and len(word) < 10:
+    word += getLetter(word[-2:])
+  return unicode(word.strip())
 
-      if isPrefix:
-        self.prefixes.append(syllable)
-      elif isSuffix:
-        self.suffixes.append(syllable)
-      else:
-        self.middles.append(syllable)
 
-  def compose(self, numSyllables):
-    """Composes a new word, returning the given number of syllables."""
+# Just for testing...
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.DEBUG)
 
-    word = []
-    word.append(self.prefixes[random.randint(0, len(self.prefixes)-1)])
-    for _ in range(1, numSyllables - 1):
-      word.append(self._getSyllable(self.middles, word[-1]))
-    word.append(self._getSyllable(self.suffixes, word[-1]))
-
-    return self._combine(word)
-
-  def _combine(self, syllables):
-    """Combines the given collection of syllables into a complete word."""
-
-    word = ""
-    for s in syllables:
-      word += s.syllable
-    return word
-
-  def _getSyllable(self, pool, prevSyllable):
-    """Looks in the given "pool" of syllables for a valid syllable to add to the list.
-
-    Note that it's possible for this method to go into an infinite loop if there are no
-    valid combinations... be careful! (TODO: fix this!)"""
-
-    while True:
-      nextSyllable = pool[random.randint(0, len(pool)-1)]
-      if self._isValidPair(prevSyllable, nextSyllable):
-        return nextSyllable
-
-  def _isValidPair(self, prevSyllable, nextSyllable):
-    """Checks whether the given two syllables are valid together or not."""
-
-    if prevSyllable.requiresNextVowel and not nextSyllable.startsWithVowel:
-      return False
-    if prevSyllable.requiresNextConsonant and nextSyllable.startsWithVowel:
-      return False
-
-    if nextSyllable.requiresPreviousVowel and not prevSyllable.endsWithVowel:
-      return False
-    if nextSyllable.requiresPreviousConsonant and prevSyllable.endsWithVowel:
-      return False
-
-    return True
-
-  def _isVowel(self, ch):
-    return ch in ['a', 'e', 'i', 'o', 'u']
-
-class _Syllable:
-  syllable = None
-  requiresNextVowel = False
-  requiresPreviousVowel = False
-  requiresNextConsonant = False
-  requiresPreviousConsonant = False
-  startsWithVowel = False
-  endsWithVowel = True
+  inf = os.path.dirname(__file__)+"/vocabulary/elvish.txt"
+  vocab = _parseVocabulary(inf)
+  for _ in range(20):
+    print _generateName(vocab)
