fe3078a9a4704f141651283104460dadafade263
Dean Harding <dean@codeka.com.au>
2013-01-16 16:06:30 +0000

    Fixed a bug with resuming activties.

--- WarWorlds-Android/src/au/com/codeka/warworlds/BaseActivity.java
+++ WarWorlds-Android/src/au/com/codeka/warworlds/BaseActivity.java
@@ -15,7 +15,7 @@ public class BaseActivity extends FragmentActivity {
     public void onResume() {
         // if we're resuming after a long break, we could have been GCed in
         // which case need to set things up again...
-        Util.getProperties();
+        Util.loadProperties(this);
         Util.setup(this);
 
         BackgroundDetector.getInstance().onActivityResume(this);
