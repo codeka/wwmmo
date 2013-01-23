eb52ad64713aab2635954be00c574e49a5b02257
Dean Harding <dean@codeka.com.au>
2013-01-23 10:41:55 +0000

    Make pressing the action key send the chat message.

--- WarWorlds-Android/src/au/com/codeka/warworlds/game/ChatActivity.java
+++ WarWorlds-Android/src/au/com/codeka/warworlds/game/ChatActivity.java
@@ -4,13 +4,16 @@ import java.util.List;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import au.com.codeka.warworlds.BaseActivity;
 import au.com.codeka.warworlds.R;
 import au.com.codeka.warworlds.model.ChatManager;
@@ -35,18 +38,39 @@ public class ChatActivity extends BaseActivity
 
         final EditText chatMsg = (EditText) findViewById(R.id.chat_text);
 
+        chatMsg.setOnEditorActionListener(new OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_NULL) {
+                    sendCurrentChat();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         Button send = (Button) findViewById(R.id.chat_send);
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ChatMessage msg = new ChatMessage();
-                msg.setMessage(chatMsg.getText().toString());
-                msg.setEmpire(EmpireManager.getInstance().getEmpire());
-                chatMsg.setText("");
-
-                ChatManager.getInstance().postMessage(msg);
+                sendCurrentChat();
             }
         });
+    }
+
+    private void sendCurrentChat() {
+        EditText chatMsg = (EditText) findViewById(R.id.chat_text);
+        if (chatMsg.getText().toString().isEmpty()) {
+            return;
+        }
+
+        ChatMessage msg = new ChatMessage();
+        msg.setMessage(chatMsg.getText().toString());
+        msg.setEmpire(EmpireManager.getInstance().getEmpire());
+        chatMsg.setText("");
+
+        ChatManager.getInstance().postMessage(msg);
+
     }
 
     @Override
