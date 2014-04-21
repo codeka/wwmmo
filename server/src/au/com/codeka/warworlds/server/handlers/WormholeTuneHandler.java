e36bac79434fbb1576f4bcc6af5ead28c3b78a7d
Dean Harding <dean@codeka.com.au>
2014-04-21 17:03:31 +1000

    Don't allow tune when the wormhole doesn't belong to you.

--- server/src/au/com/codeka/warworlds/server/handlers/WormholeTuneHandler.java
+++ server/src/au/com/codeka/warworlds/server/handlers/WormholeTuneHandler.java
@@ -3,8 +3,11 @@ package au.com.codeka.warworlds.server.handlers;
 import au.com.codeka.common.protobuf.Messages;
 import au.com.codeka.warworlds.server.RequestException;
 import au.com.codeka.warworlds.server.RequestHandler;
+import au.com.codeka.warworlds.server.ctrl.EmpireController;
 import au.com.codeka.warworlds.server.ctrl.StarController;
 import au.com.codeka.warworlds.server.ctrl.WormholeController;
+import au.com.codeka.warworlds.server.model.Alliance;
+import au.com.codeka.warworlds.server.model.Empire;
 import au.com.codeka.warworlds.server.model.Star;
 
 public class WormholeTuneHandler extends RequestHandler {
@@ -25,6 +28,28 @@ public class WormholeTuneHandler extends RequestHandler {
         Star destWormhole = new StarController().getStar(pb.getDestStarId());
         if (destWormhole.getStarType().getType() != Star.Type.Wormhole) {
             throw new RequestException(400);
+        }
+
+        Star.WormholeExtra srcWormholeExtra = srcWormhole.getWormholeExtra();
+        Star.WormholeExtra destWormholeExtra = destWormhole.getWormholeExtra();
+
+        Empire empire = new EmpireController().getEmpire(getSession().getEmpireID());
+        if (srcWormholeExtra.getEmpireID() != empire.getID()) {
+            Empire ownerEmpire = new EmpireController().getEmpire(srcWormholeExtra.getEmpireID());
+            if (ownerEmpire.getAlliance() == null ||
+                    empire.getAlliance() == null ||
+                    ((Alliance) ownerEmpire.getAlliance()).getID() != ((Alliance) empire.getAlliance()).getID()) {
+                throw new RequestException(400, "You do not have control of this wormhole and cannot tune it.");
+            }
+        }
+
+        if (destWormholeExtra.getEmpireID() != empire.getID()) {
+            Empire ownerEmpire = new EmpireController().getEmpire(destWormholeExtra.getEmpireID());
+            if (ownerEmpire.getAlliance() == null ||
+                    empire.getAlliance() == null ||
+                    ((Alliance) ownerEmpire.getAlliance()).getID() != ((Alliance) empire.getAlliance()).getID()) {
+                throw new RequestException(400, "You do not have control of the destination wormhole and cannot tune it.");
+            }
         }
 
         new WormholeController().tuneWormhole(srcWormhole, destWormhole);
