8c7b99d376e44a22e7b507f3996002ba7d2a4cfe
Dean Harding <dean@codeka.com.au>
2014-12-25 22:47:23 +1100

    For #85, got release builds working and signed.

--- client/src/au/com/codeka/warworlds/game/empire/BaseFragment.java
+++ client/src/au/com/codeka/warworlds/game/empire/BaseFragment.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import au.com.codeka.warworlds.R;
 
-class BaseFragment extends Fragment {
+public class BaseFragment extends Fragment {
     /** Gets a view to display if we're still loading the empire details. */
     protected View getLoadingView(LayoutInflater inflator) {
         return inflator.inflate(R.layout.empire_loading_tab, null);
