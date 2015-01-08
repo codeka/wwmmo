0846b8294bbdfd11f09d115b9019fd093fb4a500
Dean Harding <dean@codeka.com.au>
2015-01-08 23:19:19 +1100

    Formatting.

--- client/src/au/com/codeka/warworlds/game/wormhole/WormholeFragment.java
+++ client/src/au/com/codeka/warworlds/game/wormhole/WormholeFragment.java
@@ -21,6 +21,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import au.com.codeka.common.TimeFormatter;
 import au.com.codeka.common.model.BaseStar;
 import au.com.codeka.warworlds.BaseGlFragment;
@@ -42,270 +43,277 @@ import au.com.codeka.warworlds.model.ShieldManager;
 import au.com.codeka.warworlds.model.Star;
 import au.com.codeka.warworlds.model.StarManager;
 
-/** This fragment is used in place of SolarSystemFragment in the SolarSystemActivity for
-    wormholes. */
+/**
+ * This fragment is used in place of SolarSystemFragment in the SolarSystemActivity for
+ * wormholes.
+ */
 public class WormholeFragment extends BaseGlFragment {
-    private WormholeSceneManager mWormhole;
-    private Star star;
-    private Star destStar;
-    private DateTime tuneCompleteTime;
-    private Handler handler;
-    private View contentView;
+  private WormholeSceneManager wormhole;
+  private Star star;
+  private Star destStar;
+  private DateTime tuneCompleteTime;
+  private Handler handler;
+  private View contentView;
 
-    @Override
-    public void onCreate(final Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        handler = new Handler();
+    handler = new Handler();
 
-        EmpireShieldManager.i.clearTextureCache();
-        StarManager.eventBus.register(eventHandler);
+    EmpireShieldManager.i.clearTextureCache();
+    StarManager.eventBus.register(eventHandler);
 
+    Bundle extras = getArguments();
+    int starID = (int) extras.getLong("au.com.codeka.warworlds.StarID");
+    wormhole = new WormholeSceneManager(WormholeFragment.this, starID);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    contentView = super.onCreateView(inflater, container, savedInstanceState);
+
+    Button renameBtn = (Button) contentView.findViewById(R.id.rename_btn);
+    renameBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        RenameDialog dialog = new RenameDialog();
+        dialog.setWormhole(star);
+        dialog.show(getFragmentManager(), "");
+      }
+    });
+
+    Button destinationBtn = (Button) contentView.findViewById(R.id.destination_btn);
+    destinationBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        DestinationDialog dialog = DestinationDialog.newInstance(star);
+        dialog.show(getActivity().getSupportFragmentManager(), "");
+      }
+    });
+
+    Button viewDestinationBtn = (Button) contentView.findViewById(R.id.view_destination_btn);
+    viewDestinationBtn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (destStar == null) {
+          return;
+        }
+
+        // This cast isn't great...
+        ((SolarSystemActivity) getActivity()).showStar(destStar.getID());
+      }
+    });
+    viewDestinationBtn.setEnabled(false);
+
+    FleetListWormhole fleetList = (FleetListWormhole) contentView.findViewById(R.id.fleet_list);
+    fleetList.setOnFleetActionListener(new FleetList.OnFleetActionListener() {
+      @Override
+      public void onFleetView(Star star, Fleet fleet) {
+        // won't be called here...
+      }
+
+      @Override
+      public void onFleetSplit(Star star, Fleet fleet) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FleetSplitDialog dialog = new FleetSplitDialog();
+        dialog.setFleet(fleet);
+        dialog.show(fm, "");
+      }
+
+      @Override
+      public void onFleetBoost(Star star, Fleet fleet) {
+        // won't be called here
+      }
+
+      @Override
+      public void onFleetMove(Star star, Fleet fleet) {
+        FleetMoveActivity.show(getActivity(), fleet);
+        ;
+      }
+
+      @Override
+      public void onFleetMerge(Fleet fleet, List<Fleet> potentialFleets) {
+        FragmentManager fm = getActivity().getSupportFragmentManager();
+        FleetMergeDialog dialog = new FleetMergeDialog();
+        dialog.setup(fleet, potentialFleets);
+        dialog.show(fm, "");
+      }
+
+      @Override
+      public void onFleetStanceModified(Star star, Fleet fleet, Fleet.Stance newStance) {
+        EmpireManager.i.getEmpire().updateFleetStance(star, fleet, newStance);
+      }
+    });
+
+    return contentView;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    ServerGreeter.waitForHello(getActivity(), new ServerGreeter.HelloCompleteHandler() {
+      @Override
+      public void onHelloComplete(boolean success, ServerGreeting greeting) {
         Bundle extras = getArguments();
         int starID = (int) extras.getLong("au.com.codeka.warworlds.StarID");
-        mWormhole = new WormholeSceneManager(WormholeFragment.this, starID);
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        contentView = super.onCreateView(inflater, container, savedInstanceState);
-
-        Button renameBtn = (Button) contentView.findViewById(R.id.rename_btn);
-        renameBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RenameDialog dialog = new RenameDialog();
-                dialog.setWormhole(star);
-                dialog.show(getFragmentManager(), "");
-            }
-        });
-
-        Button destinationBtn = (Button) contentView.findViewById(R.id.destination_btn);
-        destinationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                DestinationDialog dialog = DestinationDialog.newInstance(star);
-                dialog.show(getActivity().getSupportFragmentManager(), "");
-            }
-        });
-
-        Button viewDestinationBtn = (Button) contentView.findViewById(R.id.view_destination_btn);
-        viewDestinationBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (destStar == null) {
-                    return;
-                }
-
-                // This cast isn't great...
-                ((SolarSystemActivity) getActivity()).showStar(destStar.getID());
-            }
-        });
-        viewDestinationBtn.setEnabled(false);
-
-        FleetListWormhole fleetList = (FleetListWormhole) contentView.findViewById(R.id.fleet_list);
-        fleetList.setOnFleetActionListener(new FleetList.OnFleetActionListener() {
-            @Override
-            public void onFleetView(Star star, Fleet fleet) {
-                // won't be called here...
-            }
-
-            @Override
-            public void onFleetSplit(Star star, Fleet fleet) {
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                FleetSplitDialog dialog = new FleetSplitDialog();
-                dialog.setFleet(fleet);
-                dialog.show(fm, "");
-            }
-
-            @Override
-            public void onFleetBoost(Star star, Fleet fleet) {
-                // won't be called here
-            }
-
-            @Override
-            public void onFleetMove(Star star, Fleet fleet) {
-                FleetMoveActivity.show(getActivity(), fleet);;
-            }
-
-            @Override
-            public void onFleetMerge(Fleet fleet, List<Fleet> potentialFleets) {
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                FleetMergeDialog dialog = new FleetMergeDialog();
-                dialog.setup(fleet, potentialFleets);
-                dialog.show(fm, "");
-            }
-
-            @Override
-            public void onFleetStanceModified(Star star, Fleet fleet, Fleet.Stance newStance) {
-                EmpireManager.i.getEmpire().updateFleetStance(star, fleet, newStance);
-            }
-        });
-
-        return contentView;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        ServerGreeter.waitForHello(getActivity(), new ServerGreeter.HelloCompleteHandler() {
-            @Override
-            public void onHelloComplete(boolean success, ServerGreeting greeting) {
-                Bundle extras = getArguments();
-                int starID = (int) extras.getLong("au.com.codeka.warworlds.StarID");
-
-                star = StarManager.i.getStar(starID);
-                if (star != null) {
-                    refreshStar();
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        StarManager.eventBus.unregister(eventHandler);
-    }
-
-    private Object eventHandler = new Object() {
-        @EventHandler
-        public void onStarFetched(Star s) {
-            Bundle extras = getArguments();
-            int starID = (int) extras.getLong("au.com.codeka.warworlds.StarID");
-
-            if (s.getID() == starID) {
-                star = s;
-                refreshStar();
-            } else if (star != null && star.getWormholeExtra().getDestWormholeID() == s.getID()) {
-                destStar = s;
-                refreshStar();
-            }
-        }
-
-        @EventHandler
-        public void onEmpireUpdated(Empire empire) {
+        star = StarManager.i.getStar(starID);
+        if (star != null) {
           refreshStar();
         }
+      }
+    });
+  }
 
-        @EventHandler
-        public void onShieldUpdated(ShieldManager.ShieldUpdatedEvent event) {
-            refreshStar();
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    StarManager.eventBus.unregister(eventHandler);
+  }
+
+  private Object eventHandler = new Object() {
+    @EventHandler
+    public void onStarFetched(Star s) {
+      Bundle extras = getArguments();
+      int starID = (int) extras.getLong("au.com.codeka.warworlds.StarID");
+
+      if (s.getID() == starID) {
+        star = s;
+        refreshStar();
+      } else if (star != null && star.getWormholeExtra().getDestWormholeID() == s.getID()) {
+        destStar = s;
+        refreshStar();
+      }
+    }
+
+    @EventHandler
+    public void onEmpireUpdated(Empire empire) {
+      refreshStar();
+    }
+
+    @EventHandler
+    public void onShieldUpdated(ShieldManager.ShieldUpdatedEvent event) {
+      refreshStar();
+    }
+  };
+
+  private void refreshStar() {
+    if (star.getWormholeExtra().getDestWormholeID() != 0 && (destStar == null || !destStar.getKey()
+        .equals(Integer.toString(star.getWormholeExtra().getDestWormholeID())))) {
+      destStar = StarManager.i.getStar(star.getWormholeExtra().getDestWormholeID());
+    }
+
+    TextView starName = (TextView) contentView.findViewById(R.id.star_name);
+    TextView destinationName = (TextView) contentView.findViewById(R.id.destination_name);
+    FleetListWormhole fleetList = (FleetListWormhole) contentView.findViewById(R.id.fleet_list);
+
+    if (destinationName.getText().toString().equals("")) {
+      destinationName.setText(Html.fromHtml("→ <i>None</i>"));
+    }
+    starName.setText(star.getName());
+
+    TreeMap<String, Star> stars = new TreeMap<String, Star>();
+    stars.put(star.getKey(), star);
+    fleetList.refresh(star.getFleets(), stars);
+
+    if (destStar != null) {
+      BaseStar.WormholeExtra wormholeExtra = star.getWormholeExtra();
+      if (wormholeExtra.getTuneCompleteTime() != null && wormholeExtra.getTuneCompleteTime()
+          .isAfter(DateTime.now())) {
+        tuneCompleteTime = wormholeExtra.getTuneCompleteTime();
+      }
+
+      String str = String.format(Locale.ENGLISH, "→ %s", destStar.getName());
+      if (tuneCompleteTime != null) {
+        str = "<font color=\"red\">" + str + "</font>";
+      } else {
+        contentView.findViewById(R.id.view_destination_btn).setEnabled(true);
+      }
+      destinationName.setText(Html.fromHtml(str));
+    }
+    updateTuningProgress();
+
+    Empire empire = EmpireManager.i.getEmpire(star.getWormholeExtra().getEmpireID());
+    if (empire != null) {
+      TextView empireName = (TextView) contentView.findViewById(R.id.empire_name);
+      empireName.setVisibility(View.VISIBLE);
+      empireName.setText(empire.getDisplayName());
+
+      Bitmap bmp = EmpireShieldManager.i.getShield(getActivity(), empire);
+      ImageView empireIcon = (ImageView) contentView.findViewById(R.id.empire_icon);
+      empireIcon.setVisibility(View.VISIBLE);
+      empireIcon.setImageBitmap(bmp);
+    } else {
+      contentView.findViewById(R.id.empire_name).setVisibility(View.GONE);
+      contentView.findViewById(R.id.empire_icon).setVisibility(View.GONE);
+    }
+  }
+
+  private void updateTuningProgress() {
+    TextView tuningProgress = (TextView) contentView.findViewById(R.id.tuning_progress);
+    if (tuneCompleteTime == null && destStar != null) {
+      tuningProgress.setText("");
+
+      String str = String.format(Locale.ENGLISH, "→ %s", destStar.getName());
+      TextView destinationName = (TextView) contentView.findViewById(R.id.destination_name);
+      destinationName.setText(Html.fromHtml(str));
+    } else if (tuneCompleteTime != null) {
+      tuningProgress.setText(String.format(Locale.ENGLISH, "%s left",
+          TimeFormatter.create().format(DateTime.now(), tuneCompleteTime)));
+
+      handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+          updateTuningProgress();
         }
-    };
-
-    private void refreshStar() {
-        if (star.getWormholeExtra().getDestWormholeID() != 0 && (
-                destStar == null || !destStar.getKey().equals(Integer.toString(star.getWormholeExtra().getDestWormholeID())))) {
-            destStar = StarManager.i.getStar(star.getWormholeExtra().getDestWormholeID());
-        }
-
-        TextView starName  = (TextView) contentView.findViewById(R.id.star_name);
-        TextView destinationName = (TextView) contentView.findViewById(R.id.destination_name);
-        FleetListWormhole fleetList = (FleetListWormhole) contentView.findViewById(R.id.fleet_list);
-
-        if (destinationName.getText().toString().equals("")) {
-            destinationName.setText(Html.fromHtml("→ <i>None</i>"));
-        }
-        starName.setText(star.getName());
-
-        TreeMap<String, Star> stars = new TreeMap<String, Star>();
-        stars.put(star.getKey(), star);
-        fleetList.refresh(star.getFleets(), stars);
-
-        if (destStar != null) {
-            BaseStar.WormholeExtra wormholeExtra = star.getWormholeExtra();
-            if (wormholeExtra.getTuneCompleteTime() != null &&
-                    wormholeExtra.getTuneCompleteTime().isAfter(DateTime.now())) {
-                tuneCompleteTime = wormholeExtra.getTuneCompleteTime();
-            }
-
-            String str = String.format(Locale.ENGLISH, "→ %s", destStar.getName());
-            if (tuneCompleteTime != null) {
-                str = "<font color=\"red\">" + str + "</font>";
-            } else {
-                contentView.findViewById(R.id.view_destination_btn).setEnabled(true);
-            }
-            destinationName.setText(Html.fromHtml(str));
-
-            updateTuningProgress();
-        }
-
-        Empire empire = EmpireManager.i.getEmpire(star.getWormholeExtra().getEmpireID());
-        if (empire != null) {
-            TextView empireName = (TextView) contentView.findViewById(R.id.empire_name);
-            empireName.setVisibility(View.VISIBLE);
-            empireName.setText(empire.getDisplayName());
-
-            Bitmap bmp = EmpireShieldManager.i.getShield(getActivity(), empire);
-            ImageView empireIcon = (ImageView) contentView.findViewById(R.id.empire_icon);
-            empireIcon.setVisibility(View.VISIBLE);
-            empireIcon.setImageBitmap(bmp);
-        } else {
-            contentView.findViewById(R.id.empire_name).setVisibility(View.GONE);
-            contentView.findViewById(R.id.empire_icon).setVisibility(View.GONE);
-        }
+      }, 10000);
+    } else {
+      tuningProgress.setText("");
     }
+  }
 
-    private void updateTuningProgress() {
-        TextView tuningProgress = (TextView) contentView.findViewById(R.id.tuning_progress);
-        if (tuneCompleteTime == null && destStar != null) {
-            tuningProgress.setText("");
+  /**
+   * Create the camera, we don't have a zoom factor.
+   */
+  @Override
+  protected Camera createCamera() {
+    ZoomCamera camera = new ZoomCamera(0, 0, mCameraWidth, mCameraHeight);
 
-            String str = String.format(Locale.ENGLISH, "→ %s", destStar.getName());
-            TextView destinationName = (TextView) contentView.findViewById(R.id.destination_name);
-            destinationName.setText(Html.fromHtml(str));
-        } else if (tuneCompleteTime != null) {
-            tuningProgress.setText(String.format(Locale.ENGLISH, "%s left",
-                    TimeFormatter.create().format(DateTime.now(), tuneCompleteTime)));
+    return camera;
+  }
 
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    updateTuningProgress();
-                }
-            }, 10000);
-        }
-    }
+  @Override
+  protected int getRenderSurfaceViewID() {
+    return R.id.wormhole;
+  }
 
-    /** Create the camera, we don't have a zoom factor. */
-    @Override
-    protected Camera createCamera() {
-        ZoomCamera camera = new ZoomCamera(0, 0, mCameraWidth, mCameraHeight);
+  @Override
+  protected int getLayoutID() {
+    return R.layout.wormhole;
+  }
 
-        return camera;
-    }
+  @Override
+  protected void onCreateResources() throws IOException {
+    wormhole.onLoadResources();
+  }
 
-    @Override
-    protected int getRenderSurfaceViewID() {
-        return R.id.wormhole;
-    }
+  @Override
+  protected Scene onCreateScene() throws IOException {
+    return wormhole.createScene();
+  }
 
-    @Override
-    protected int getLayoutID() {
-        return R.layout.wormhole;
-    }
+  @Override
+  public void onStart() {
+    super.onStart();
+    wormhole.onStart();
+  }
 
-    @Override
-    protected void onCreateResources() throws IOException {
-        mWormhole.onLoadResources();
-    }
-
-    @Override
-    protected Scene onCreateScene() throws IOException {
-        return mWormhole.createScene();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mWormhole.onStart();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mWormhole.onStop();
-    }
+  @Override
+  public void onStop() {
+    super.onStop();
+    wormhole.onStop();
+  }
 }
