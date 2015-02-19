65372fa9cf19fcc6642e7d72283fe64a0da39337
Dean Harding <dean@codeka.com.au>
2015-02-19 21:37:43 +1100

    Make sure the file names are sorted.

--- planet-render-test/src/main/java/au/com/codeka/planetrendertest/AppContent.java
+++ planet-render-test/src/main/java/au/com/codeka/planetrendertest/AppContent.java
@@ -14,6 +14,7 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 import javax.swing.DefaultComboBoxModel;
@@ -276,7 +277,9 @@ public class AppContent extends JPanel {
     }
 
     String[] array = new String[fileNames.size()];
-    return fileNames.toArray(array);
+    fileNames.toArray(array);
+    Arrays.sort(array);
+    return array;
   }
 
   private String loadTemplate(String name) {
