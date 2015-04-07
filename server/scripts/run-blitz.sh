2cd617cd12e2f8d554190112674f83022d66cdd1
Dean Harding <dean@codeka.com.au>
2015-04-07 21:06:33 +1000

    Don't do any daemonization stuff ourselves, we'll let an external program convert us to a daemon.

--- server/scripts/run-blitz.sh
+++ server/scripts/run-blitz.sh
@@ -2,22 +2,9 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-PIDFILE=""
-
-OPTIND=1
-while getopts "p:" opt; do
-  case "$opt" in
-    p)
-      PIDFILE=$OPTARG
-      ;;
-  esac
-done
-shift "$((OPTIND-1))"
-
 pushd $DIR
 SERVER_OPTS=""
 SERVER_OPTS="$SERVER_OPTS -Dau.com.codeka.warworlds.server.ConfigFile=data/config-blitz.json"
 SERVER_OPTS="$SERVER_OPTS -Djava.util.logging.config.file=logging-blitz.properties"
-SERVER_OPTS="$SERVER_OPTS" nohup ./bin/server $* &
-echo "$!" > $PIDFILE
+SERVER_OPTS="$SERVER_OPTS" ./bin/server $*
 popd
