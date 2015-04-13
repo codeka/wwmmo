0b3588f2d1bf729a611d877805af8d900f406f27
Dean Harding <dean@codeka.com.au>
2015-04-13 21:33:35 +1000

    Reformat.

--- server/src/au/com/codeka/warworlds/server/ctrl/SessionController.java
+++ server/src/au/com/codeka/warworlds/server/ctrl/SessionController.java
@@ -12,97 +12,101 @@ import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
+import org.joda.time.DateTime;
+
+import javax.annotation.Nonnull;
+
 public class SessionController {
-    private static LoadingCache<String, Session> sSessionCache;
-    private static LoadingCache<String, Integer> sEmpireIDCache;
+  private static final LoadingCache<String, Session> SESSION_CACHE;
+  private static final LoadingCache<String, Integer> EMPIRE_ID_CACHE;
 
-    static {
-        sSessionCache = CacheBuilder.newBuilder()
-                .expireAfterAccess(24, TimeUnit.HOURS)
-                .maximumSize(5000)
-                .build(new CacheLoader<String, Session>() {
-                    @Override
-                    public Session load(String cookie) throws Exception {
-                        try (SqlStmt stmt = DB.prepare("SELECT * FROM sessions WHERE session_cookie=?")) {
-                            stmt.setString(1, cookie);
-                            SqlResult res = stmt.select();
-                            if (res.next()) {
-                                return new Session(res);
-                            }
-                        }
-
-                        throw new RequestException(403, "Could not find session, session cookie: "+cookie);
-                    }
-                });
-
-        sEmpireIDCache = CacheBuilder.newBuilder()
-                .expireAfterAccess(24, TimeUnit.HOURS)
-                .maximumSize(10000)
-                .build(new CacheLoader<String, Integer>() {
-                    @Override
-                    public Integer load(String email) throws Exception {
-                        try (SqlStmt stmt = DB.prepare("SELECT id FROM empires WHERE user_email = ?")) {
-                            stmt.setString(1, email);
-                            SqlResult res = stmt.select();
-                            if (res.next()) {
-                                return res.getInt(1);
-                            }
-                        }
-
-                        return null;
-                    }
-                });
-    }
-
-    public void saveSession(Session session) throws RequestException {
-        sSessionCache.put(session.getCookie(), session);
-
-        String sql = "DELETE FROM sessions WHERE session_cookie = ?";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            stmt.setString(1, session.getCookie());
-            stmt.update();
-        } catch (Exception e) {
-            throw new RequestException(e);
-        }
-
-        sql = "INSERT INTO sessions (session_cookie, user_email, login_time, empire_id," +
-              " alliance_id, is_admin, inline_notifications)" +
-              " VALUES (?, ?, ?, ?, ?, ?, 0)";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            stmt.setString(1, session.getCookie());
-            stmt.setString(2, session.getActualEmail());
-            stmt.setDateTime(3, session.getLoginTime());
-            if (session.getEmpireID() == 0) {
-                stmt.setNull(4);
-            } else {
-                stmt.setInt(4, session.getEmpireID());
-            }
-            if (session.getAllianceID() == 0) {
-                stmt.setNull(5);
-            } else {
-                stmt.setInt(5, session.getAllianceID());
-            }
-            stmt.setInt(6, session.isAdmin() ? 1 : 0);
-            stmt.update();
-        } catch (Exception e) {
-            throw new RequestException(e);
-        }
-    }
-
-    public Session getSession(String cookie, String impersonateEmail) throws RequestException {
-        try {
-            Session session = sSessionCache.get(cookie);
-
-            if (impersonateEmail != null) {
-                Integer empireID = sEmpireIDCache.get(impersonateEmail);
-                if (empireID != null) {
-                    session = session.impersonate(impersonateEmail, empireID);
-                }
+  static {
+    SESSION_CACHE = CacheBuilder.newBuilder()
+        .expireAfterAccess(24, TimeUnit.HOURS)
+        .maximumSize(5000)
+        .build(new CacheLoader<String, Session>() {
+          @Override
+          public Session load(@Nonnull String cookie) throws Exception {
+            try (SqlStmt stmt = DB.prepare("SELECT * FROM sessions WHERE session_cookie=?")) {
+              stmt.setString(1, cookie);
+              SqlResult res = stmt.select();
+              if (res.next()) {
+                return new Session(res);
+              }
             }
 
-            return session;
-        } catch (Exception e) {
-            throw new RequestException(e);
-        }
+            throw new RequestException(403, "Could not find session, session cookie: " + cookie);
+          }
+        });
+
+    EMPIRE_ID_CACHE = CacheBuilder.newBuilder()
+        .expireAfterAccess(24, TimeUnit.HOURS)
+        .maximumSize(10000)
+        .build(new CacheLoader<String, Integer>() {
+          @Override
+          public Integer load(@Nonnull String email) throws Exception {
+            try (SqlStmt stmt = DB.prepare("SELECT id FROM empires WHERE user_email = ?")) {
+              stmt.setString(1, email);
+              SqlResult res = stmt.select();
+              if (res.next()) {
+                return res.getInt(1);
+              }
+            }
+
+            return null;
+          }
+        });
+  }
+
+  public void saveSession(Session session) throws RequestException {
+    SESSION_CACHE.put(session.getCookie(), session);
+
+    String sql = "DELETE FROM sessions WHERE session_cookie = ?";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      stmt.setString(1, session.getCookie());
+      stmt.update();
+    } catch (Exception e) {
+      throw new RequestException(e);
     }
+
+    sql = "INSERT INTO sessions (session_cookie, user_email, login_time, empire_id," +
+        " alliance_id, is_admin, inline_notifications)" +
+        " VALUES (?, ?, ?, ?, ?, ?, 0)";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      stmt.setString(1, session.getCookie());
+      stmt.setString(2, session.getActualEmail());
+      stmt.setDateTime(3, session.getLoginTime());
+      if (session.getEmpireID() == 0) {
+        stmt.setNull(4);
+      } else {
+        stmt.setInt(4, session.getEmpireID());
+      }
+      if (session.getAllianceID() == 0) {
+        stmt.setNull(5);
+      } else {
+        stmt.setInt(5, session.getAllianceID());
+      }
+      stmt.setInt(6, session.isAdmin() ? 1 : 0);
+      stmt.update();
+    } catch (Exception e) {
+      throw new RequestException(e);
+    }
+  }
+
+  public Session getSession(String cookie, String impersonateEmail) throws RequestException {
+    try {
+      Session session = SESSION_CACHE.get(cookie);
+
+      if (impersonateEmail != null) {
+        Integer empireID = EMPIRE_ID_CACHE.get(impersonateEmail);
+        if (empireID != null) {
+          session = session.impersonate(impersonateEmail, empireID);
+        }
+      }
+
+      return session;
+    } catch (Exception e) {
+      throw new RequestException(e);
+    }
+  }
 }
