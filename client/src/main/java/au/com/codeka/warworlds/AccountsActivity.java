b17672ecf6ee506f573da6057e6c118445b3de86
Dean Harding <dean@codeka.com.au>
2015-04-14 20:16:33 +1000

    Reformat.

--- client/src/main/java/au/com/codeka/warworlds/AccountsActivity.java
+++ client/src/main/java/au/com/codeka/warworlds/AccountsActivity.java
@@ -1,4 +1,3 @@
-
 package au.com.codeka.warworlds;
 
 import java.util.ArrayList;
@@ -15,7 +14,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -25,121 +23,109 @@ import android.widget.TextView;
  * Account selections activity - handles device registration and unregistration.
  */
 public class AccountsActivity extends BaseActivity {
-    private int mAccountSelectedPosition = 0;
-    private Context mContext = this;
+  private int accountSelectedPosition = 0;
+  private Context context = this;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        View rootView = findViewById(android.R.id.content);
-        ActivityBackgroundGenerator.setBackground(rootView);
+    View rootView = findViewById(android.R.id.content);
+    ActivityBackgroundGenerator.setBackground(rootView);
 
-        Util.setup(this);
-        Util.loadProperties();
-        SharedPreferences prefs = Util.getSharedPreferences();
-        String accountName = prefs.getString("AccountName", null);
+    Util.setup(this);
+    Util.loadProperties();
+    SharedPreferences prefs = Util.getSharedPreferences();
+    String accountName = prefs.getString("AccountName", null);
 
-        setContentView(R.layout.accounts);
+    setContentView(R.layout.accounts);
 
-        List<String> accounts = getGoogleAccounts();
-        if (accounts.size() == 0) {
-            // Show a dialog and invoke the "Add Account" activity if requested
-            StyledDialog.Builder builder = new StyledDialog.Builder(mContext);
-            builder.setMessage("You need a Google Account in order to be able to play War Worlds.");
-            builder.setPositiveButton("Add Account", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    startActivity(new Intent(Settings.ACTION_ADD_ACCOUNT));
-                }
-            });
-            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int which) {
-                    // TODO: the whole game should exit...
-                    finish();
-                }
-            });
-            builder.setTitle("No Google Account");
-            builder.create().show();
-        } else {
-            if (accountName != null) {
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).equals(accountName)) {
-                        mAccountSelectedPosition = i;
-                        break;
-                    }
-                }
-            }
-
-            ListView listView = (ListView) findViewById(R.id.select_account);
-            listView.setAdapter(new ArrayAdapter<String>(mContext, R.layout.account, accounts));
-            listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-            listView.setItemChecked(mAccountSelectedPosition, true);
+    List<String> accounts = getGoogleAccounts();
+    if (accounts.size() == 0) {
+      // Show a dialog and invoke the "Add Account" activity if requested
+      StyledDialog.Builder builder = new StyledDialog.Builder(context);
+      builder.setMessage("You need a Google Account in order to be able to play War Worlds.");
+      builder.setPositiveButton("Add Account", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+          startActivity(new Intent(Settings.ACTION_ADD_ACCOUNT));
         }
-
-        final ListView listView = (ListView) findViewById(R.id.select_account);
-        final Button logInButton = (Button) findViewById(R.id.log_in_btn);
-        logInButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                // Get account name
-                mAccountSelectedPosition = listView.getCheckedItemPosition();
-                TextView account = (TextView) listView.getChildAt(mAccountSelectedPosition);
-
-                // Register, and force another 'hello'
-                register(account.getText().toString());
-                if (RealmContext.i.getCurrentRealm() != null) {
-                    RealmContext.i.getCurrentRealm().getAuthenticator().logout();
-                }
-                ServerGreeter.clearHello();
-
-                finish();
-            }
-        });
-
-        ((Button) findViewById(R.id.help_btn)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.war-worlds.com/doc/getting-started"));
-                startActivity(i);
-            }
-        });
-
-        ((Button) findViewById(R.id.privacy_policy_btn)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_VIEW);
-                i.setData(Uri.parse("http://www.war-worlds.com/privacy-policy"));
-                startActivity(i);
-            }
-        });
-    }
-
-    /**
-     * Registers for C2DM messaging with the given account name.
-     * 
-     * @param accountName a String containing a Google account name
-     */
-    private void register(final String accountName) {
-        // Store the account name in shared preferences
-        final SharedPreferences prefs = Util.getSharedPreferences();
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("AccountName", accountName);
-        editor.commit();
-    }
-
-    /**
-     * Returns a list of registered Google account names. If no Google accounts
-     * are registered on the device, a zero-length list is returned.
-     */
-    private List<String> getGoogleAccounts() {
-        ArrayList<String> result = new ArrayList<String>();
-        Account[] accounts = AccountManager.get(mContext).getAccounts();
-        for (Account account : accounts) {
-            if (account.type.equals("com.google")) {
-                result.add(account.name);
-            }
+      });
+      builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+          finish();
         }
+      });
+      builder.setTitle("No Google Account");
+      builder.create().show();
+    } else {
+      if (accountName != null) {
+        for (int i = 0; i < accounts.size(); i++) {
+          if (accounts.get(i).equals(accountName)) {
+            accountSelectedPosition = i;
+            break;
+          }
+        }
+      }
 
-        return result;
+      ListView listView = (ListView) findViewById(R.id.select_account);
+      listView.setAdapter(new ArrayAdapter<>(context, R.layout.account, accounts));
+      listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+      listView.setItemChecked(accountSelectedPosition, true);
     }
+
+    final ListView listView = (ListView) findViewById(R.id.select_account);
+    final Button logInButton = (Button) findViewById(R.id.log_in_btn);
+    logInButton.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        // Get account name
+        accountSelectedPosition = listView.getCheckedItemPosition();
+        TextView account = (TextView) listView.getChildAt(accountSelectedPosition);
+
+        // Save the new account name, and force another 'hello'
+        Util.getSharedPreferences().edit()
+            .putString("AccountName", account.getText().toString())
+            .apply();
+        if (RealmContext.i.getCurrentRealm() != null) {
+          RealmContext.i.getCurrentRealm().getAuthenticator().logout();
+        }
+        ServerGreeter.clearHello();
+
+        finish();
+      }
+    });
+
+    findViewById(R.id.help_btn).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("http://www.war-worlds.com/doc/getting-started"));
+        startActivity(i);
+      }
+    });
+
+    findViewById(R.id.privacy_policy_btn).setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("http://www.war-worlds.com/privacy-policy"));
+        startActivity(i);
+      }
+    });
+  }
+
+  /**
+   * Returns a list of registered Google account names. If no Google accounts are registered on the
+   * device, a zero-length list is returned.
+   */
+  private List<String> getGoogleAccounts() {
+    ArrayList<String> result = new ArrayList<>();
+    Account[] accounts = AccountManager.get(context).getAccounts();
+    for (Account account : accounts) {
+      if (account.type.equals("com.google")) {
+        result.add(account.name);
+      }
+    }
+
+    return result;
+  }
 }
