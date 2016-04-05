eb4713cf976468c86dd1605a6567de66adeff6eb
Dean Harding <dean@codeka.com.au>
2016-04-05 20:38:54 +1000

    Pin the new certificate as well.

--- client/src/main/java/au/com/codeka/warworlds/api/RequestManager.java
+++ client/src/main/java/au/com/codeka/warworlds/api/RequestManager.java
@@ -50,6 +50,8 @@ public class RequestManager {
     httpClient = new OkHttpClient();
 
     CertificatePinner certificatePinner = new CertificatePinner.Builder()
+        .add("game.war-worlds.com", "sha1/7pk8GWNOfZdQYlUboW2S4aXDNls=")
+        // The below are obsolete as of May, 2016.
         .add("game.war-worlds.com", "sha1/DQYffq7Bm+6ChL1D0VUBHAd3k6g=")
         .add("game.war-worlds.com", "sha1/o5OZxATDsgmwgcIfIWIneMJ0jkw=")
         .build();
