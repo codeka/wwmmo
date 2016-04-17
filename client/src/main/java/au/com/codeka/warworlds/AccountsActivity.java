9c17c1e4f029106cf382d4510b28b860d60e55ec
Dean Harding <dean@codeka.com.au>
2016-04-17 12:09:08 +1000

    Request Contacts permissions so that we can get the list of accounts on M+ devices.

--- client/src/main/java/au/com/codeka/warworlds/AccountsActivity.java
+++ client/src/main/java/au/com/codeka/warworlds/AccountsActivity.java
@@ -3,15 +3,19 @@ package au.com.codeka.warworlds;
 import java.util.ArrayList;
 import java.util.List;
 
+import android.*;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.text.Html;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,6 +37,8 @@ public class AccountsActivity extends BaseActivity {
   private int accountSelectedPosition = 0;
   private Context context = this;
 
+  private final int GET_ACCOUNTS_PERMISSION = 1;
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -43,45 +49,6 @@ public class AccountsActivity extends BaseActivity {
     Util.setup(this);
     Util.loadProperties();
     setContentView(R.layout.accounts);
-
-    SharedPreferences prefs = Util.getSharedPreferences();
-    String accountName = prefs.getString("AccountName", null);
-    if (accountName != null && !accountName.endsWith("@anon.war-worlds.com")) {
-      findViewById(R.id.anon_overwrite_notice).setVisibility(View.GONE);
-    }
-
-    List<String> accounts = getGoogleAccounts();
-    if (accounts.size() == 0) {
-      // Show a dialog and invoke the "Add Account" activity if requested
-      StyledDialog.Builder builder = new StyledDialog.Builder(context);
-      builder.setMessage("You need a Google Account in order to be able to play War Worlds.");
-      builder.setPositiveButton("Add Account", new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-          startActivity(new Intent(Settings.ACTION_ADD_ACCOUNT));
-        }
-      });
-      builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-          finish();
-        }
-      });
-      builder.setTitle("No Google Account");
-      builder.create().show();
-    } else {
-      if (accountName != null) {
-        for (int i = 0; i < accounts.size(); i++) {
-          if (accounts.get(i).equals(accountName)) {
-            accountSelectedPosition = i;
-            break;
-          }
-        }
-      }
-
-      ListView listView = (ListView) findViewById(R.id.select_account);
-      listView.setAdapter(new ArrayAdapter<>(context, R.layout.account, accounts));
-      listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-      listView.setItemChecked(accountSelectedPosition, true);
-    }
 
     final ListView listView = (ListView) findViewById(R.id.select_account);
     final Button logInButton = (Button) findViewById(R.id.log_in_btn);
@@ -125,6 +92,86 @@ public class AccountsActivity extends BaseActivity {
     });
   }
 
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    SharedPreferences prefs = Util.getSharedPreferences();
+    String accountName = prefs.getString("AccountName", null);
+    if (accountName != null && !accountName.endsWith("@anon.war-worlds.com")) {
+      findViewById(R.id.anon_overwrite_notice).setVisibility(View.GONE);
+    }
+
+    List<String> accounts = getGoogleAccounts();
+    if (accounts.size() == 0) {
+      // If we don't have GET_ACCOUNTS permission, we'll want to request that and try again.
+      int permissionCheck = ContextCompat.checkSelfPermission(
+          AccountsActivity.this, android.Manifest.permission.GET_ACCOUNTS);
+      if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+        // Show a dialog and invoke the "Add Account" activity if requested
+        StyledDialog.Builder builder = new StyledDialog.Builder(context);
+        builder.setMessage("In order to fetch accounts on your phone, we need to ask for 'Contacts'"
+            + " permission. War Worlds doesn't actually need access to your Contacts, but"
+            + " because of the way permissions are grouped, this is how we have to ask for it.");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            ActivityCompat.requestPermissions(AccountsActivity.this,
+                new String[]{android.Manifest.permission.GET_ACCOUNTS},
+                GET_ACCOUNTS_PERMISSION);
+            dialog.dismiss();
+          }
+        });
+        builder.setTitle("Permissions");
+        builder.create().show();
+      } else {
+        // Show a dialog and invoke the "Add Account" activity if requested
+        StyledDialog.Builder builder = new StyledDialog.Builder(context);
+        builder.setMessage("You need a Google Account in order to be able to play War Worlds.");
+        builder.setPositiveButton("Add Account", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            startActivity(new Intent(Settings.ACTION_ADD_ACCOUNT));
+          }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            finish();
+          }
+        });
+        builder.setTitle("No Google Account");
+        builder.create().show();
+      }
+    } else {
+      if (accountName != null) {
+        for (int i = 0; i < accounts.size(); i++) {
+          if (accounts.get(i).equals(accountName)) {
+            accountSelectedPosition = i;
+            break;
+          }
+        }
+      }
+
+      ListView listView = (ListView) findViewById(R.id.select_account);
+      listView.setAdapter(new ArrayAdapter<>(context, R.layout.account, accounts));
+      listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+      listView.setItemChecked(accountSelectedPosition, true);
+    }
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+      int requestCode, String permissions[], int[] grantResults) {
+    switch (requestCode) {
+      case GET_ACCOUNTS_PERMISSION: {
+        // If request is cancelled, the result arrays are empty.
+        if (grantResults.length > 0
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+          // TODO: granted, do we need to do anything else?
+        } else {
+          finish();
+        }
+      }
+    }
+  }
   /** Associates the current anonymous empire with the given new account name. */
   private void associateAnonAccount(final String newAccountName) {
     RequestManager.i.sendRequest(new ApiRequest.Builder("anon-associate", "POST")
