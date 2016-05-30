77716989bebfc6e7f1924fe84428f94cb3a1a4ae
Dean Harding <dean@codeka.com.au>
2016-05-30 23:18:08 +1000

    Updated the starfield to include empire details.

--- server/src/main/data/admin/static/js/empirestore.js
+++ server/src/main/data/admin/static/js/empirestore.js
@@ -10,7 +10,10 @@ var empireStore = (function() {
 
   function defaultCallback(empire) {
     $("span[data-empireid]").each(function(i, elem) {
-      $(this).html(empire.display_name);
+      var $this = $(this);
+      if ($this.data("empireid") == empire.id) {
+        $this.html(empire.display_name);
+      }
     });
   }
 
@@ -24,20 +27,20 @@ var empireStore = (function() {
 
       // If we have a cached empire already, just return that and we're done.
       if (typeof empires[empireId] != "undefined") {
-        callback(empires[empireId]);
+        setTimeout(function() { callback(empires[empireId]); }, 0);
         return;
       }
 
       // If we have one stored, we can return that for now, but we'll want to re-fetch from the
       // server anyway, to ensure we have the freshest.
       if (typeof storedEmpires[empireId] != "undefined") {
-        callback(storedEmpires[empireId]);
+        setTimeout(function() { callback(empires[empireId]); }, 0);
       }
 
       // If we already have a callback for this empireId, it means we're already fetching it, so
       // just add to the list for when we get the fresh data.
       if (typeof callbacks[empireId] != "undefined") {
-        callbacks[empireId].push(callback);
+        setTimeout(function() { callback(empires[empireId]); }, 0);
         return;
       }
 
--- server/src/main/data/admin/static/js/starfield.js
+++ server/src/main/data/admin/static/js/starfield.js
@@ -29,6 +29,41 @@ $(function() {
       label.data("star", star);
       label.on("click", function() { showStar($(this).data("star")); });
       container.append(label);
+
+      var empires = [];
+      for (var i in star.planets) {
+        var planet = star.planets[i];
+        if (planet.colony != null && planet.colony.empire_id != null) {
+          if (empires.indexOf(planet.colony.empire_id) === -1) {
+            empires.push(planet.colony.empire_id);
+          }
+        }
+      }
+      for (var i in star.fleets) {
+        var fleet = star.fleets[i];
+        if (fleet.empire_id != null) {
+          if (empires.indexOf(fleet.empire_id) === -1) {
+            empires.push(fleet.empire_id);
+          }
+        }
+      }
+      for (var i in empires) {
+        label = $("<div class=\"starfield-empire\"><img /><span>...</span></div>");
+        $("span", label).attr("data-empireid", empires[i]);
+        var angle = (Math.PI / 4.0) * (i + 1);
+        var x = 0.0;
+        var y = -20.0;
+        var nx = (x * Math.cos(angle) - y * Math.sin(angle));
+        var ny = (y * Math.cos(angle) + x * Math.sin(angle));
+        label.css("left", parseInt(star.offset_x * 0.5 + nx) + "px");
+        label.css("top", parseInt(star.offset_y * 0.5  + ny) + "px");
+        $("img", label)
+            .attr("src", "/render/empire/" + empires[i] + "/16x16/mdpi.png")
+            .css("vertical-align", "bottom")
+            .css("margin-right", "2px");
+        container.append(label);
+        empireStore.getEmpire(empires[i]);
+      }
     }
   }
 
