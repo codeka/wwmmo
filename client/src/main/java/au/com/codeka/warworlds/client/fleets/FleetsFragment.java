1d216d49b30e54ab0497f0321566c3fa81420b08
Dean Harding <dean@codeka.com.au>
2016-06-22 21:06:23 +1000

    Some minor stuff.

--- client/src/main/java/au/com/codeka/warworlds/client/fleets/FleetsFragment.java
+++ client/src/main/java/au/com/codeka/warworlds/client/fleets/FleetsFragment.java
@@ -1,13 +1,16 @@
 package au.com.codeka.warworlds.client.fleets;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 
 import au.com.codeka.warworlds.client.R;
 import au.com.codeka.warworlds.client.activity.BaseFragment;
 import au.com.codeka.warworlds.common.Log;
 
 /**
- * This fragment contains a list of fleets
+ * This fragment contains a list of fleets, and lets you do all the interesting stuff on them (like
+ * merge, split, move, etc).
  */
 public class FleetsFragment extends BaseFragment {
   private static final Log log = new Log("FleetFragment");
@@ -22,5 +25,10 @@ public class FleetsFragment extends BaseFragment {
   @Override
   protected int getViewResourceId() {
     return R.layout.frag_fleets;
+  }
+
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
   }
 }
