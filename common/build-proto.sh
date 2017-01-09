0ea619a09473482aa3c90d8230cad7b5321e1029
Dean Harding <dean@codeka.com.au>
2017-01-09 21:18:44 +1100

    Remove the generated proto files, and use gradle to generate them in the future.

--- common/build-proto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-java -jar libs/wire-compiler-2.1.2-jar-with-dependencies.jar \
-     --proto_path=src/main/proto \
-     --java_out=src/main/java \
-     empire.proto star.proto packets.proto account.proto \
-     fleet.proto colony.proto design.proto admin.proto
-
