e1369723dc4a08d2833d5570dedd22bd14430d4d
Dean Harding <dean@codeka.com.au>
2017-01-28 19:05:03 +1100

    Save stars empires as well.

--- server/src/main/java/au/com/codeka/warworlds/server/store/StarsStore.java
+++ server/src/main/java/au/com/codeka/warworlds/server/store/StarsStore.java
@@ -1,13 +1,20 @@
 package au.com.codeka.warworlds.server.store;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import javax.annotation.Nullable;
 
 import au.com.codeka.warworlds.common.Log;
+import au.com.codeka.warworlds.common.proto.Fleet;
+import au.com.codeka.warworlds.common.proto.Planet;
 import au.com.codeka.warworlds.common.proto.Star;
 import au.com.codeka.warworlds.server.store.base.BaseStore;
 import au.com.codeka.warworlds.server.store.base.QueryResult;
+import au.com.codeka.warworlds.server.store.base.StoreWriter;
+import au.com.codeka.warworlds.server.store.base.Transaction;
 
 /**
  * A store for storing stars, including some extra indices for special queries that we can do.
@@ -33,23 +40,59 @@ public class StarsStore extends BaseStore {
   }
 
   public void put(long id, Star star) {
-    try {
-      newWriter()
-          .stmt("INSERT OR REPLACE INTO stars (id, sector_x, sector_y, last_simulation, star) VALUES (?, ?, ?, ?, ?)")
+    Set<Long> empireIds = new HashSet<>();
+    for (Fleet fleet : star.fleets) {
+      if (fleet.empire_id != null) {
+        empireIds.add(fleet.empire_id);
+      }
+    }
+    for (Planet planet : star.planets) {
+      if (planet.colony != null && planet.colony.empire_id != null) {
+        empireIds.add(planet.colony.empire_id);
+      }
+    }
+
+    try(Transaction trans = newTransaction()) {
+      newWriter(trans)
+          .stmt("INSERT OR REPLACE INTO stars (id, sector_x, sector_y, next_simulation, star) VALUES (?, ?, ?, ?, ?)")
           .param(0, id)
           .param(1, star.sector_x)
           .param(2, star.sector_y)
-          .param(3, star.last_simulation)
+          .param(3, star.next_simulation)
           .param(4, star.encode())
           .execute();
-    } catch (StoreException e) {
+
+      newWriter(trans)
+          .stmt("DELETE FROM star_empires WHERE star_id = ?")
+          .param(0, id)
+          .execute();
+
+      StoreWriter writer = newWriter()
+          .stmt("INSERT INTO star_empires (empire_id, star_id) VALUES (?, ?)")
+          .param(1, id);
+      for (Long empireId : empireIds) {
+        writer.param(0, empireId)
+            .execute();
+      }
+
+      trans.commit();
+    } catch (Exception e) {
       log.error("Unexpected.", e);
     }
   }
 
   @Nullable
   public Star nextStarForSimulate() {
-    // TODO
+    try (
+        QueryResult res = newReader()
+            .stmt("SELECT star FROM stars WHERE next_simulation IS NOT NULL ORDER BY next_simulation ASC")
+            .query()) {
+      if (res.next()) {
+        return Star.ADAPTER.decode(res.getBytes(0));
+      }
+    } catch (Exception e) {
+      log.error("Unexpected.", e);
+    }
     return null;
   }
 
@@ -70,8 +113,21 @@ public class StarsStore extends BaseStore {
     }
   }
 
-  public Iterable<Long> getStarsForEmpire(long empireId) {
-    return new ArrayList<>();
+  public ArrayList<Long> getStarsForEmpire(long empireId) {
+    try (
+        QueryResult res = newReader()
+            .stmt("SELECT star_id FROM star_empires WHERE empire_id = ?")
+            .param(0, empireId)
+            .query()) {
+      ArrayList<Long> ids = new ArrayList<>();
+      while (res.next()) {
+        ids.add(res.getLong(0));
+      }
+      return ids;
+    } catch (Exception e) {
+      log.error("Unexpected.", e);
+      return new ArrayList<>();
+    }
   }
 
   @Override
@@ -83,15 +139,24 @@ public class StarsStore extends BaseStore {
                   "  id INTEGER PRIMARY KEY," +
                   "  sector_x INTEGER," +
                   "  sector_y INTEGER," +
-                  "  last_simulation INTEGER," +
+                  "  next_simulation INTEGER," +
                   "  star BLOB)")
           .execute();
       newWriter()
           .stmt("CREATE INDEX IX_stars_sector ON stars (sector_x, sector_y)")
           .execute();
       newWriter()
-          .stmt("CREATE INDEX IX_stars_last_simulation ON stars (last_simulation)")
+          .stmt("CREATE INDEX IX_stars_next_simulation ON stars (next_simulation)")
           .execute();
+
+      newWriter()
+          .stmt("CREATE TABLE star_empires (empire_id INTEGER, star_id INTEGER)")
+          .execute();
+      newWriter()
+          .stmt("CREATE INDEX IX_star_empires ON star_empires (empire_id, star_id)");
+      newWriter()
+          .stmt("CREATE INDEX IX_empire_stars ON star_empires (star_id, empire_id)");
+
       diskVersion++;
     }
 
