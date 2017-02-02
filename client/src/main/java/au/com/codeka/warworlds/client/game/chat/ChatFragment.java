c00c86ff14104abf27eaf6e7ffae1778d0cc60a9
Dean Harding <dean@codeka.com.au>
2017-02-02 21:45:54 +1100

    Added a bunch of UI for chat messages.

--- client/src/main/java/au/com/codeka/warworlds/client/game/chat/ChatFragment.java
+++ client/src/main/java/au/com/codeka/warworlds/client/game/chat/ChatFragment.java
@@ -1,15 +1,11 @@
 package au.com.codeka.warworlds.client.game.chat;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
@@ -18,7 +14,6 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 
 import au.com.codeka.warworlds.client.R;
 import au.com.codeka.warworlds.client.activity.BaseFragment;
@@ -64,7 +59,7 @@ public class ChatFragment extends BaseFragment {
   public void onResume() {
     super.onResume();
 
-    refreshConversations();
+    refreshRooms();
 /*
     Bundle extras = getIntent().getExtras();
     if (extras != null) {
@@ -137,26 +132,9 @@ public class ChatFragment extends BaseFragment {
  //   }
   };
 
-  private void refreshConversations() {
-/*    mConversations = ChatManager.i.getConversations();
-    // remove the recent conversation, we don't display it here
-    Iterator<ChatConversation> it = mConversations.iterator();
-    while (it.hasNext()) {
-      ChatConversation conversation = it.next();
-      if (conversation.getID() < 0 &&
-          conversation.getID() != ChatManager.ALLIANCE_CONVERSATION_ID) {
-        it.remove();
-      }
-    }
-    if (EmpireManager.i.getEmpire().getAlliance() != null && mConversations.size() > 1) {
-      // swap alliance and global around...
-      ChatConversation globalConversation = mConversations.get(1);
-      mConversations.set(1, mConversations.get(0));
-      mConversations.set(0, globalConversation);
-    }
-
-    mChatPagerAdapter.refresh(mConversations);
-*/
+  private void refreshRooms() {
+    rooms = ChatManager.i.getRooms();
+    chatPagerAdapter.refresh(rooms);
   }
 
   public class ChatPagerAdapter extends FragmentStatePagerAdapter {
@@ -174,16 +152,21 @@ public class ChatFragment extends BaseFragment {
 
     @Override
     public Fragment getItem(int index) {
-//      Fragment fragment = new ChatFragment();
-//      Bundle args = new Bundle();
-//      args.putInt("au.com.codeka.warworlds.ConversationID", rooms.get(index).id);
-//      fragment.setArguments(args);
-//      return fragment;
-      return null;
+      Fragment fragment = new RoomFragment();
+      fragment.setArguments(RoomFragment.createArguments(rooms.get(index)));
+      return fragment;
     }
 
     @Override
     public int getItemPosition(Object item) {
+      if (item instanceof ChatRoom) {
+        ChatRoom room = (ChatRoom) item;
+        for (int i = 0; i < rooms.size(); i++) {
+          if (rooms.get(i).id.equals(room.id)) {
+            return i;
+          }
+        }
+      }
       return POSITION_NONE;
     }
 
@@ -194,16 +177,15 @@ public class ChatFragment extends BaseFragment {
 
     @Override
     public CharSequence getPageTitle(int position) {
-      ChatRoom room = rooms.get(position);
-      return String.format(Locale.US, "Chat #%d", room.id);
+      return rooms.get(position).name;
     }
 
     @Override
     public void setPrimaryItem(ViewGroup container, int position, Object object) {
       super.setPrimaryItem(container, position, object);
 
-      //ChatRoom room = rooms.get(position);
-      //conversation.markAllRead();
+      ChatRoom room = rooms.get(position);
+      // room.markAllRead()
     }
   }
 
@@ -212,11 +194,10 @@ public class ChatFragment extends BaseFragment {
       return;
     }
 
-    //ChatRoom room = rooms.get(viewPager.getCurrentItem());
-
+    ChatRoom room = rooms.get(viewPager.getCurrentItem());
     ChatMessage.Builder chatMessageBuilder = new ChatMessage.Builder()
         .message(chatMsg.getText().toString())
-        .room_id(null /* room.id */);
+        .room_id(room.id);
 
     // if this is our first chat after the update ...
 //    if (!Util.getSharedPreferences().getBoolean("au.com.codeka.warworlds.ChatAskedAboutTranslation", false)) {
--- client/src/main/java/au/com/codeka/warworlds/client/game/world/ChatManager.java
+++ client/src/main/java/au/com/codeka/warworlds/client/game/world/ChatManager.java
@@ -2,10 +2,13 @@ package au.com.codeka.warworlds.client.game.world;
 
 import com.google.common.collect.Lists;
 
+import java.util.List;
+
 import au.com.codeka.warworlds.client.App;
 import au.com.codeka.warworlds.client.util.eventbus.EventHandler;
 import au.com.codeka.warworlds.common.proto.ChatMessage;
 import au.com.codeka.warworlds.common.proto.ChatMessagesPacket;
+import au.com.codeka.warworlds.common.proto.ChatRoom;
 import au.com.codeka.warworlds.common.proto.Packet;
 
 /**
@@ -20,6 +23,26 @@ public class ChatManager {
 
   public void create() {
     // TODO?
+  }
+
+  /** Get the list of rooms that we're in. */
+  public List<ChatRoom> getRooms() {
+    // TODO: get all the rooms, not just the global one.
+    return Lists.newArrayList(new ChatRoom.Builder()
+        .id(null)
+        .name("Global")
+        .build());
+  }
+
+  public List<ChatMessage> getMessages(ChatRoom room) {
+    return Lists.newArrayList(
+        new ChatMessage.Builder()
+            .empire_id(null)
+            .message("Hello World")
+            .date_posted(System.currentTimeMillis())
+            .action(ChatMessage.MessageAction.Normal)
+            .build()
+    );
   }
 
   /** Send the given {@link ChatMessage} to the server. */
