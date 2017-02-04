39c500e09eba11eb60cb0b589a4e4f62be035e12
Dean Harding <dean@codeka.com.au>
2017-02-05 08:59:34 +1100

    Display stuff in the fleet bottom pane.

--- client/src/main/java/au/com/codeka/warworlds/client/game/starfield/FleetSelectedBottomPane.java
+++ client/src/main/java/au/com/codeka/warworlds/client/game/starfield/FleetSelectedBottomPane.java
@@ -3,11 +3,15 @@ package au.com.codeka.warworlds.client.game.starfield;
 import android.content.Context;
 import android.widget.RelativeLayout;
 
+import au.com.codeka.warworlds.client.R;
+
 /**
  * Bottom pane for when you have a fleet selected.
  */
 public class FleetSelectedBottomPane extends RelativeLayout {
   public FleetSelectedBottomPane(Context context) {
     super(context);
+
+    inflate(context, R.layout.starfield_bottom_pane_fleet, this);
   }
 }
