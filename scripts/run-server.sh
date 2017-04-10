03301d018d582eb61e47fc207940a2691481a10e
Dean Harding <dean@codeka.com.au>
2017-04-10 22:12:37 +1000

    Update dependencies, and get run-server.sh working again, since I'm affected by https://code.google.com/p/android/issues/detail?id=355434 and can't run it from the IDE.

--- scripts/run-server.sh
+++ scripts/run-server.sh
@@ -6,16 +6,17 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOTPATH=`dirname $SCRIPTPATH`
 INSTALLPATH=$ROOTPATH/server/build/install/server
+RUNPATH=$ROOTPATH/server/src/main
 
 pushd $ROOTPATH > /dev/null
-./gradlew --daemon :server:installApp
+./gradlew --daemon :server:installDist
 popd > /dev/null
 
-pushd $INSTALLPATH > /dev/null
+pushd $RUNPATH > /dev/null
 SERVER_OPTS=""
-SERVER_OPTS="$SERVER_OPTS -Dau.com.codeka.warworlds.server.ConfigFile=$INSTALLPATH/data/config-debug.json"
-SERVER_OPTS="$SERVER_OPTS -Djava.util.logging.config.file=logging-debug.properties"
-SERVER_OPTS="$SERVER_OPTS" exec ./bin/server $*
+SERVER_OPTS="$SERVER_OPTS -DConfigFile=$RUNPATH/data/config-debug.json"
+SERVER_OPTS="$SERVER_OPTS -Djava.util.logging.config.file=$RUNPATH/logging.properties"
+SERVER_OPTS="$SERVER_OPTS" exec $INSTALLPATH/bin/server $*
 popd > /dev/null
 
 
