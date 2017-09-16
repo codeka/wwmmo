8ba6608c06fba5722818efc07edc0a88b8c65cfd
Dean Harding <dean@codeka.com.au>
2017-09-16 10:21:13 +1000

    Updated the website to point to gremlin's paypal.

--- website/handlers/__init__.py
+++ website/handlers/__init__.py
@@ -14,7 +14,7 @@ import ctrl.tmpl
 
 # This value gets incremented every time we deploy so that we can cache bust
 # our static resources (css, js, etc)
-RESOURCE_VERSION = 85
+RESOURCE_VERSION = 86
 
 
 class BaseHandler(webapp.RequestHandler):
