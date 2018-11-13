9d887b46f9a1d22c7423633c582cd3edc3386c7d
Dean Harding <dean@codeka.com.au>
2018-11-12 23:03:16 -0800

    Reformatted.

--- server/src/au/com/codeka/warworlds/server/cron/UpdateRanksCronJob.java
+++ server/src/au/com/codeka/warworlds/server/cron/UpdateRanksCronJob.java
@@ -15,149 +15,149 @@ import au.com.codeka.warworlds.server.model.EmpireRank;
  * Updates the ranks of empires.
  */
 public class UpdateRanksCronJob extends CronJob {
-    @Override
-    public void run(String extra) throws Exception {
-        TreeMap<Integer, EmpireRank> ranks = new TreeMap<>();
+  @Override
+  public void run(String extra) throws Exception {
+    TreeMap<Integer, EmpireRank> ranks = new TreeMap<>();
 
-        String sql = "SELECT id AS empire_id FROM empires WHERE state <> 2";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            SqlResult res = stmt.select();
-            while (res.next()) {
-                EmpireRank rank = new EmpireRank(res);
-                ranks.put(rank.getEmpireID(), rank);
-            }
-        }
-
-        sql = "SELECT empire_id, SUM(num_ships) " +
-              "FROM fleets WHERE empire_id IS NOT NULL GROUP BY empire_id";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            SqlResult res = stmt.select();
-            while (res.next()) {
-                int empireID = res.getInt(1);
-                long totalShips = res.getLong(2);
-                if (!ranks.containsKey(empireID)) {
-                    continue;
-                }
-                ranks.get(empireID).setTotalShips(totalShips);
-            }
-        }
-
-        sql = "SELECT empire_id, COUNT(*) FROM buildings GROUP BY empire_id";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            SqlResult res = stmt.select();
-            while (res.next()) {
-                int empireID = res.getInt(1);
-                long totalBuildings = res.getLong(2);
-                if (!ranks.containsKey(empireID)) {
-                    continue;
-                }
-                ranks.get(empireID).setTotalBuildings(totalBuildings);
-            }
-        }
-
-        sql = "SELECT empire_id, COUNT(*), SUM(population) " +
-              "FROM colonies WHERE empire_id IS NOT NULL GROUP BY empire_id";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            SqlResult res = stmt.select();
-            while (res.next()) {
-                int empireID = res.getInt(1);
-                long totalColonies = res.getLong(2);
-                long totalPopulation = res.getLong(3);
-                if (!ranks.containsKey(empireID)) {
-                    continue;
-                }
-                ranks.get(empireID).setTotalColonies(totalColonies);
-                ranks.get(empireID).setTotalPopulation(totalPopulation);
-            }
-        }
-
-        sql = "SELECT empire_id, COUNT(*) FROM (" +
-               " SELECT empire_id, star_id" +
-               " FROM stars" +
-               " INNER JOIN colonies ON colonies.star_id = stars.id" +
-               " WHERE colonies.empire_id IS NOT NULL" +
-               " GROUP BY empire_id, star_id" +
-              ") AS stars" +
-             " GROUP BY empire_id";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            SqlResult res = stmt.select();
-            while (res.next()) {
-                int empireID = res.getInt(1);
-                long totalStars = res.getLong(2);
-                if (!ranks.containsKey(empireID)) {
-                    continue;
-                }
-                ranks.get(empireID).setTotalStars(totalStars);
-            }
-        }
-
-        ArrayList<EmpireRank> sortedRanks = new ArrayList<>(ranks.values());
-        sortedRanks.sort((left, right) -> {
-            long diff = right.getTotalPopulation() - left.getTotalPopulation();
-
-            if (diff != 0)
-                return (int) diff;
-
-            diff = right.getTotalColonies() - left.getTotalColonies();
-            if (diff != 0)
-                return (int) diff;
-
-            diff = right.getTotalStars() - left.getTotalStars();
-            if (diff != 0)
-                return (int) diff;
-
-            diff = right.getTotalShips() - left.getTotalShips();
-            return (int) diff;
-        });
-
-        try (Transaction t = DB.beginTransaction()) {
-            sql = "DELETE FROM empire_ranks WHERE empire_id = ?";
-            try (SqlStmt stmt = t.prepare(sql)) {
-                for (EmpireRank rank : sortedRanks) {
-                    stmt.setInt(1, rank.getEmpireID());
-                    stmt.update();
-                }
-            }
-
-            sql = "INSERT INTO empire_ranks (empire_id, rank, total_stars, total_colonies," +
-                                           " total_buildings, total_ships, total_population)" +
-                 " VALUES (?, ?, ?, ?, ?, ?, ?);";
-            try (SqlStmt stmt = t.prepare(sql)) {
-                int rankValue = 1;
-                for (EmpireRank rank : sortedRanks) {
-                    stmt.setInt(1, rank.getEmpireID());
-                    stmt.setInt(2, rankValue);
-                    stmt.setLong(3, rank.getTotalStars());
-                    stmt.setLong(4, rank.getTotalColonies());
-                    stmt.setLong(5, rank.getTotalBuildings());
-                    stmt.setLong(6, rank.getTotalShips());
-                    stmt.setLong(7, rank.getTotalPopulation());
-                    stmt.update();
-
-                    rankValue ++;
-                }
-            }
-
-            t.commit();
-        }
-
-        // Also update the alliance's "total_stars" field while we're here.
-        sql = " UPDATE beta.alliances" +
-              " SET" +
-              "   total_stars=sub.total_stars" +
-              " FROM (" +
-              "   SELECT" +
-              "     alliances.id," +
-              "     empire_ranks.total_stars" +
-              "   FROM beta.alliances" +
-              "   INNER JOIN beta.empires" +
-              "     ON alliances.id = empires.alliance_id" +
-              "   INNER JOIN beta.empire_ranks" +
-              "     ON empires.id = empire_ranks.empire_id" +
-              " ) AS sub" +
-              " WHERE alliances.id = sub.id";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            stmt.update();
-        }
+    String sql = "SELECT id AS empire_id FROM empires WHERE state <> 2";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      SqlResult res = stmt.select();
+      while (res.next()) {
+        EmpireRank rank = new EmpireRank(res);
+        ranks.put(rank.getEmpireID(), rank);
+      }
     }
+
+    sql = "SELECT empire_id, SUM(num_ships) " +
+        "FROM fleets WHERE empire_id IS NOT NULL GROUP BY empire_id";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      SqlResult res = stmt.select();
+      while (res.next()) {
+        int empireID = res.getInt(1);
+        long totalShips = res.getLong(2);
+        if (!ranks.containsKey(empireID)) {
+          continue;
+        }
+        ranks.get(empireID).setTotalShips(totalShips);
+      }
+    }
+
+    sql = "SELECT empire_id, COUNT(*) FROM buildings GROUP BY empire_id";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      SqlResult res = stmt.select();
+      while (res.next()) {
+        int empireID = res.getInt(1);
+        long totalBuildings = res.getLong(2);
+        if (!ranks.containsKey(empireID)) {
+          continue;
+        }
+        ranks.get(empireID).setTotalBuildings(totalBuildings);
+      }
+    }
+
+    sql = "SELECT empire_id, COUNT(*), SUM(population) " +
+        "FROM colonies WHERE empire_id IS NOT NULL GROUP BY empire_id";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      SqlResult res = stmt.select();
+      while (res.next()) {
+        int empireID = res.getInt(1);
+        long totalColonies = res.getLong(2);
+        long totalPopulation = res.getLong(3);
+        if (!ranks.containsKey(empireID)) {
+          continue;
+        }
+        ranks.get(empireID).setTotalColonies(totalColonies);
+        ranks.get(empireID).setTotalPopulation(totalPopulation);
+      }
+    }
+
+    sql = "SELECT empire_id, COUNT(*) FROM (" +
+        " SELECT empire_id, star_id" +
+        " FROM stars" +
+        " INNER JOIN colonies ON colonies.star_id = stars.id" +
+        " WHERE colonies.empire_id IS NOT NULL" +
+        " GROUP BY empire_id, star_id" +
+        ") AS stars" +
+        " GROUP BY empire_id";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      SqlResult res = stmt.select();
+      while (res.next()) {
+        int empireID = res.getInt(1);
+        long totalStars = res.getLong(2);
+        if (!ranks.containsKey(empireID)) {
+          continue;
+        }
+        ranks.get(empireID).setTotalStars(totalStars);
+      }
+    }
+
+    ArrayList<EmpireRank> sortedRanks = new ArrayList<>(ranks.values());
+    sortedRanks.sort((left, right) -> {
+      long diff = right.getTotalPopulation() - left.getTotalPopulation();
+
+      if (diff != 0)
+        return (int) diff;
+
+      diff = right.getTotalColonies() - left.getTotalColonies();
+      if (diff != 0)
+        return (int) diff;
+
+      diff = right.getTotalStars() - left.getTotalStars();
+      if (diff != 0)
+        return (int) diff;
+
+      diff = right.getTotalShips() - left.getTotalShips();
+      return (int) diff;
+    });
+
+    try (Transaction t = DB.beginTransaction()) {
+      sql = "DELETE FROM empire_ranks WHERE empire_id = ?";
+      try (SqlStmt stmt = t.prepare(sql)) {
+        for (EmpireRank rank : sortedRanks) {
+          stmt.setInt(1, rank.getEmpireID());
+          stmt.update();
+        }
+      }
+
+      sql = "INSERT INTO empire_ranks (empire_id, rank, total_stars, total_colonies," +
+          " total_buildings, total_ships, total_population)" +
+          " VALUES (?, ?, ?, ?, ?, ?, ?);";
+      try (SqlStmt stmt = t.prepare(sql)) {
+        int rankValue = 1;
+        for (EmpireRank rank : sortedRanks) {
+          stmt.setInt(1, rank.getEmpireID());
+          stmt.setInt(2, rankValue);
+          stmt.setLong(3, rank.getTotalStars());
+          stmt.setLong(4, rank.getTotalColonies());
+          stmt.setLong(5, rank.getTotalBuildings());
+          stmt.setLong(6, rank.getTotalShips());
+          stmt.setLong(7, rank.getTotalPopulation());
+          stmt.update();
+
+          rankValue++;
+        }
+      }
+
+      t.commit();
+    }
+
+    // Also update the alliance's "total_stars" field while we're here.
+    sql = " UPDATE beta.alliances" +
+        " SET" +
+        "   total_stars=sub.total_stars" +
+        " FROM (" +
+        "   SELECT" +
+        "     alliances.id," +
+        "     empire_ranks.total_stars" +
+        "   FROM beta.alliances" +
+        "   INNER JOIN beta.empires" +
+        "     ON alliances.id = empires.alliance_id" +
+        "   INNER JOIN beta.empire_ranks" +
+        "     ON empires.id = empire_ranks.empire_id" +
+        " ) AS sub" +
+        " WHERE alliances.id = sub.id";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      stmt.update();
+    }
+  }
 }
