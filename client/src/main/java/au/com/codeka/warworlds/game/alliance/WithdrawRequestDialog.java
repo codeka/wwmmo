6f5725407318ad22d78c2765a21ab6f36ed9e504
Dean Harding <dean@codeka.com.au>
2018-11-15 19:31:13 -0800

    Don't dismiss the dialog, just ignore NumberFormatException.

--- client/src/main/java/au/com/codeka/warworlds/game/alliance/WithdrawRequestDialog.java
+++ client/src/main/java/au/com/codeka/warworlds/game/alliance/WithdrawRequestDialog.java
@@ -14,42 +14,48 @@ import au.com.codeka.warworlds.StyledDialog;
 import au.com.codeka.warworlds.model.AllianceManager;
 
 public class WithdrawRequestDialog extends DialogFragment {
-    private View mView;
-    private int mAllianceID;
+  private View mView;
+  private int mAllianceID;
 
-    public void setAllianceID(int allianceID) {
-        mAllianceID = allianceID;
+  public void setAllianceID(int allianceID) {
+    mAllianceID = allianceID;
+  }
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    final Activity activity = getActivity();
+    LayoutInflater inflater = activity.getLayoutInflater();
+    mView = inflater.inflate(R.layout.alliance_request_dlg, null);
+
+    TextView instructions = mView.findViewById(R.id.instructions);
+    instructions.setText("Enter the amount to withdraw, and the reason for wanting to withdraw " +
+        "cash, then click \"Withdraw\". Your request must be approved before the cash is given " +
+        "to you.");
+
+    return new StyledDialog.Builder(getActivity())
+        .setView(mView)
+        .setTitle("Withdraw Cash")
+        .setPositiveButton("Withdraw", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            onWithdraw();
+          }
+        })
+        .setNegativeButton("Cancel", null)
+        .create();
+  }
+
+  private void onWithdraw() {
+    TextView message = mView.findViewById(R.id.message);
+    TextView amount = mView.findViewById(R.id.amount);
+    try {
+      AllianceManager.i.requestWithdraw(mAllianceID, message.getText().toString(),
+          Integer.parseInt(amount.getText().toString()));
+    } catch (NumberFormatException e) {
+      return;
     }
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final Activity activity = getActivity();
-        LayoutInflater inflater = activity.getLayoutInflater();
-        mView = inflater.inflate(R.layout.alliance_request_dlg, null);
-
-        TextView instructions = (TextView) mView.findViewById(R.id.instructions);
-        instructions.setText("Enter the amount to withdraw, and the reason for wanting to withdraw cash, then click \"Withdraw\". Your request must be approved before the cash is given to you.");
-
-        return new StyledDialog.Builder(getActivity())
-                           .setView(mView)
-                           .setTitle("Withdraw Cash")
-                           .setPositiveButton("Withdraw", new DialogInterface.OnClickListener() {
-                               @Override
-                               public void onClick(DialogInterface dialog, int which) {
-                                   onWithdraw();
-                               }
-                           })
-                           .setNegativeButton("Cancel", null)
-                           .create();
-    }
-
-    private void onWithdraw() {
-        TextView message = (TextView) mView.findViewById(R.id.message);
-        TextView amount = (TextView) mView.findViewById(R.id.amount);
-        AllianceManager.i.requestWithdraw(mAllianceID, message.getText().toString(),
-                            Integer.parseInt(amount.getText().toString()));
-
-        dismiss();
-    }
+    dismiss();
+  }
 }
 
