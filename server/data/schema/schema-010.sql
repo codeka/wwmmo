ac825bd9a580c9aff1c8e74aff0fd744d765047a
Dean Harding <dean@codeka.com.au>
2019-04-28 19:37:29 -0700

    Modify the index so it works the way we want it to for wormhole queries.

--- server/data/schema/schema-010.sql
+++ server/data/schema/schema-010.sql
@@ -2,4 +2,4 @@
 ALTER TABLE stars
   ADD COLUMN wormhole_empire_id BIGINT;
 
-CREATE INDEX stars_wormhole_empire_id ON stars (wormhole_empire_id);
+CREATE INDEX stars_wormhole_star_type_empire_id ON stars (star_type, wormhole_empire_id);
