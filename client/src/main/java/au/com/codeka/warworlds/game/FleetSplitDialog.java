0a19151d79141169d005d25d4683f61741c90b58
Dean Harding <dean@codeka.com.au>
2019-04-29 22:29:43 -0700

    Don't crash when deleting the text.

--- client/src/main/java/au/com/codeka/warworlds/game/FleetSplitDialog.java
+++ client/src/main/java/au/com/codeka/warworlds/game/FleetSplitDialog.java
@@ -9,6 +9,9 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
+
+import java.util.Locale;
+
 import au.com.codeka.BackgroundRunner;
 import au.com.codeka.common.protobuf.Messages;
 import au.com.codeka.warworlds.R;
@@ -20,134 +23,147 @@ import au.com.codeka.warworlds.model.Fleet;
 import au.com.codeka.warworlds.model.StarManager;
 
 public class FleetSplitDialog extends DialogFragment {
-    private Fleet mFleet;
-    private View mView;
+  private Fleet fleet;
+  private View view;
 
-    public FleetSplitDialog() {
+  public FleetSplitDialog() {
+  }
+
+  public void setFleet(Fleet fleet) {
+    this.fleet = fleet;
+  }
+
+  @Override
+  public Dialog onCreateDialog(Bundle savedInstanceState) {
+    if (fleet == null) {
+      return null;
     }
 
-    public void setFleet(Fleet fleet) {
-        mFleet = fleet;
-    }
+    LayoutInflater inflater = getActivity().getLayoutInflater();
+    view = inflater.inflate(R.layout.fleet_split_dlg, null);
 
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        if (mFleet == null) {
-            return null;
+    final SeekBar splitRatio = view.findViewById(R.id.split_ratio);
+    final TextView splitLeft = view.findViewById(R.id.split_left);
+    final TextView splitRight = view.findViewById(R.id.split_right);
+
+    FleetListRow fleetView = view.findViewById(R.id.fleet);
+    fleetView.setFleet(fleet);
+
+    int numShips = (int) Math.ceil(fleet.getNumShips());
+    if (numShips >= 2) {
+      splitRatio.setMax(numShips - 2);
+    } else {
+      splitRatio.setMax(numShips - 1);
+    }
+    splitRatio.setProgress(numShips / 2 - 1);
+    splitLeft.setText(String.format(Locale.ENGLISH, "%d", numShips / 2));
+    splitRight.setText(String.format(Locale.ENGLISH, "%d", numShips - (numShips / 2)));
+
+    splitRatio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+      }
+
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        splitLeft.setText(String.format(Locale.ENGLISH, "%d", progress + 1));
+        splitRight.setText(String.format(Locale.ENGLISH, "%d", seekBar.getMax() - progress + 1));
+      }
+    });
+
+    splitLeft.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+      @Override
+      public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+          try {
+            int val = Integer.parseInt(splitLeft.getText().toString());
+            if (val < 0) {
+              val = 0;
+            }
+            if (val > splitRatio.getMax()) {
+              val = splitRatio.getMax();
+            }
+            splitRatio.setProgress(val - 1);
+          } catch (NumberFormatException e) {
+            // Something went wrong trying to parse the value. We'll assume it was "1"
+            splitRatio.setProgress(0);
+          }
         }
+      }
+    });
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        mView = inflater.inflate(R.layout.fleet_split_dlg, null);
-
-        final SeekBar splitRatio = (SeekBar) mView.findViewById(R.id.split_ratio);
-        final TextView splitLeft = (TextView) mView.findViewById(R.id.split_left);
-        final TextView splitRight = (TextView) mView.findViewById(R.id.split_right);
-
-        FleetListRow fleetView = (FleetListRow) mView.findViewById(R.id.fleet);
-        fleetView.setFleet(mFleet);
-
-        int numShips = (int) Math.ceil(mFleet.getNumShips());
-        if (numShips >= 2) {
-            splitRatio.setMax(numShips - 2);
-        } else {
-            splitRatio.setMax(numShips - 1);
+    splitRight.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+      @Override
+      public void onFocusChange(View v, boolean hasFocus) {
+        if (!hasFocus) {
+          try {
+            int val = Integer.parseInt(splitLeft.getText().toString());
+            if (val < 0) {
+              val = 0;
+            }
+            if (val > splitRatio.getMax()) {
+              val = splitRatio.getMax();
+            }
+            splitRatio.setProgress(splitRatio.getMax() - val + 1);
+          } catch (NumberFormatException e) {
+            splitRatio.setProgress(splitRatio.getMax());
+          }
         }
-        splitRatio.setProgress(numShips / 2 - 1);
-        splitLeft.setText(Integer.toString(numShips / 2));
-        splitRight.setText(Integer.toString(numShips - (numShips / 2)));
+      }
+    });
 
-        splitRatio.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                splitLeft.setText(Integer.toString(progress + 1));
-                splitRight.setText(Integer.toString(seekBar.getMax() - progress + 1));
-            }
-        });
+    StyledDialog.Builder b = new StyledDialog.Builder(getActivity());
+    b.setView(view);
+    b.setTitle("Split Fleet");
 
-        splitLeft.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    int val = Integer.parseInt(splitLeft.getText().toString());
-                    if (val < 0) {
-                        val = 0;
-                    }
-                    if (val > splitRatio.getMax()) {
-                        val = splitRatio.getMax();
-                    }
-                    splitRatio.setProgress(val - 1);
-                }
-            }
-        });
+    b.setPositiveButton("Split", new DialogInterface.OnClickListener() {
+      @Override
+      public void onClick(DialogInterface dialog, int which) {
+        onSplitClick();
+      }
+    });
 
-        splitRight.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (!hasFocus) {
-                    int val = Integer.parseInt(splitLeft.getText().toString());
-                    if (val < 0) {
-                        val = 0;
-                    }
-                    if (val > splitRatio.getMax()) {
-                        val = splitRatio.getMax();
-                    }
-                    splitRatio.setProgress(splitRatio.getMax() - val + 1);
-                }
-            }
-        });
+    b.setNegativeButton("Cancel", null);
 
-        StyledDialog.Builder b = new StyledDialog.Builder(getActivity());
-        b.setView(mView);
-        b.setTitle("Split Fleet");
+    return b.create();
+  }
 
-        b.setPositiveButton("Split", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                onSplitClick();
-            }
-        });
+  private void onSplitClick() {
+    ((StyledDialog) getDialog()).setCloseable(false);
 
-        b.setNegativeButton("Cancel", null);
+    final TextView splitLeft = view.findViewById(R.id.split_left);
+    final TextView splitRight = view.findViewById(R.id.split_right);
+    dismiss();
 
-        return b.create();
-    }
+    new BackgroundRunner<Boolean>() {
+      @Override
+      protected Boolean doInBackground() {
+        String url = String.format("stars/%s/fleets/%s/orders",
+            fleet.getStarKey(),
+            fleet.getKey());
+        Messages.FleetOrder fleetOrder = Messages.FleetOrder.newBuilder()
+            .setOrder(Messages.FleetOrder.FLEET_ORDER.SPLIT)
+            .setSplitLeft(Integer.parseInt(splitLeft.getText().toString()))
+            .setSplitRight(Integer.parseInt(splitRight.getText().toString()))
+            .build();
 
-    private void onSplitClick() {
-        ((StyledDialog) getDialog()).setCloseable(false);
+        try {
+          return ApiClient.postProtoBuf(url, fleetOrder);
+        } catch (ApiException e) {
+          // TODO: do something..?
+          return false;
+        }
+      }
 
-        final TextView splitLeft = (TextView) mView.findViewById(R.id.split_left);
-        final TextView splitRight = (TextView) mView.findViewById(R.id.split_right);
-        dismiss();
-
-        new BackgroundRunner<Boolean>() {
-            @Override
-            protected Boolean doInBackground() {
-                String url = String.format("stars/%s/fleets/%s/orders",
-                                           mFleet.getStarKey(),
-                                           mFleet.getKey());
-                Messages.FleetOrder fleetOrder = Messages.FleetOrder.newBuilder()
-                               .setOrder(Messages.FleetOrder.FLEET_ORDER.SPLIT)
-                               .setSplitLeft(Integer.parseInt(splitLeft.getText().toString()))
-                               .setSplitRight(Integer.parseInt(splitRight.getText().toString()))
-                               .build();
-
-                try {
-                    return ApiClient.postProtoBuf(url, fleetOrder);
-                } catch (ApiException e) {
-                    // TODO: do something..?
-                    return false;
-                }
-            }
-
-            @Override
-            protected void onComplete(Boolean success) {
-                // the star this fleet is attached to needs to be refreshed...
-                StarManager.i.refreshStar(Integer.parseInt(mFleet.getStarKey()));
-            }
-        }.execute();
-    }
+      @Override
+      protected void onComplete(Boolean success) {
+        // the star this fleet is attached to needs to be refreshed...
+        StarManager.i.refreshStar(Integer.parseInt(fleet.getStarKey()));
+      }
+    }.execute();
+  }
 }
