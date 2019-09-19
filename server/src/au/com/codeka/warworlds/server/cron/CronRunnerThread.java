b193551b0b9e207333351d677330638196b2b3e7
Dean Harding <dean@codeka.com.au>
2019-09-18 22:48:35 -0700

    Temporarily just run some stuff every hour.

--- server/src/au/com/codeka/warworlds/server/cron/CronRunnerThread.java
+++ server/src/au/com/codeka/warworlds/server/cron/CronRunnerThread.java
@@ -1,12 +1,35 @@
 package au.com.codeka.warworlds.server.cron;
 
+import java.util.concurrent.TimeUnit;
+
 import au.com.codeka.common.Log;
+import au.com.codeka.warworlds.server.cron.jobs.UpdateDashboardCronJob;
+import au.com.codeka.warworlds.server.cron.jobs.UpdateRanksCronJob;
 
 public class CronRunnerThread extends Thread {
   private static final Log log = new Log("CronRunnerThread");
 
   /** Called at startup to begin the cron thread. */
   public static void setup() {
-    // TODO
+    CronRunnerThread thread = new CronRunnerThread();
+    thread.start();
+  }
+
+  @Override
+  public void run() {
+    while (true) {
+      try {
+        new UpdateRanksCronJob().run("");
+        new UpdateDashboardCronJob().run("");
+      } catch (Exception e) {
+        log.error("Error running jobs.", e);
+      }
+
+      try {
+        Thread.sleep(TimeUnit.HOURS.toMillis(1));
+      } catch (InterruptedException e) {
+        return;
+      }
+    }
   }
 }
