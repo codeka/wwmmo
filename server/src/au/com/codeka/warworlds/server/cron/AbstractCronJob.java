c9c158cd510096a005d1d5c841479d3d13aee343
Dean Harding <dean@codeka.com.au>
2019-09-18 22:47:24 -0700

    Added some code to list all of the cron types.

--- server/src/au/com/codeka/warworlds/server/cron/AbstractCronJob.java
+++ server/src/au/com/codeka/warworlds/server/cron/AbstractCronJob.java
@@ -1,8 +1,17 @@
 package au.com.codeka.warworlds.server.cron;
 
+import org.reflections.Reflections;
+
+import java.util.Set;
+
 /**
  * This is the base class for all cron jobs.
  */
 public abstract class AbstractCronJob {
   public abstract void run(String extra) throws Exception;
+
+  public static Set<Class<?>> findAllJobs() {
+    return new Reflections("au.com.codeka.warworlds.server")
+        .getTypesAnnotatedWith(CronJob.class);
+  }
 }
--- server/src/au/com/codeka/warworlds/server/handlers/admin/AdminCronHandler.java
+++ server/src/au/com/codeka/warworlds/server/handlers/admin/AdminCronHandler.java
@@ -1,8 +1,41 @@
 package au.com.codeka.warworlds.server.handlers.admin;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+import au.com.codeka.warworlds.server.RequestException;
+import au.com.codeka.warworlds.server.cron.AbstractCronJob;
+import au.com.codeka.warworlds.server.cron.CronJob;
+
 /**
  * Handles requests for /admin/cron. Shows the list of cron jobs, lets you register new ones, run
  * jobs immediately and show the last status of the jobs.
  */
-public class AdminCronHandler extends AdminGenericHandler {
+public class AdminCronHandler extends AdminHandler {
+  @Override
+  public void get() throws RequestException {
+    if (!isAdmin()) {
+      return;
+    }
+
+    TreeMap<String, Object> data = new TreeMap<>();
+
+    ArrayList<JobDetails> jobs = new ArrayList<>();
+    for (Class<?> jobClass : AbstractCronJob.findAllJobs()) {
+      jobs.add(new JobDetails(jobClass));
+    }
+    data.put("jobs", jobs);
+
+    render("admin/cron/index.html", data);
+  }
+
+  public static class JobDetails {
+    public CronJob annotation;
+    public String className;
+
+    public JobDetails(Class<?> cls) {
+      this.annotation = cls.getAnnotation(CronJob.class);
+      this.className = cls.getName();
+    }
+  }
 }
