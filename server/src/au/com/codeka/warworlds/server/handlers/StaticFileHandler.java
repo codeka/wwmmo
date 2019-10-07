f4fae34be12b992203793e9fb7799a32f5a31050
Dean Harding <dean@codeka.com.au>
2019-10-06 23:40:27 -0700

    Convert file-not-founds into 404 errors.

--- server/src/au/com/codeka/warworlds/server/handlers/StaticFileHandler.java
+++ server/src/au/com/codeka/warworlds/server/handlers/StaticFileHandler.java
@@ -12,41 +12,41 @@ import au.com.codeka.warworlds.server.RequestException;
 import au.com.codeka.warworlds.server.RequestHandler;
 
 public class StaticFileHandler extends RequestHandler {
-    private final Log log = new Log("AdminGenericHandler");
+  private final Log log = new Log("AdminGenericHandler");
 
-    @Override
-    protected void get() throws RequestException {
-        String path = getExtraOption() + getUrlParameter("path");
+  @Override
+  protected void get() throws RequestException {
+    String path = getExtraOption() + getUrlParameter("path");
 
-        String contentType = "text/plain";
-        if (path.endsWith(".css")) {
-            contentType = "text/css";
-        } else if (path.endsWith(".js")) {
-            contentType = "text/javascript";
-        } else if (path.endsWith(".png")) {
-            contentType = "image/png";
-        } else if (path.endsWith(".ico")) {
-            contentType = "image/x-icon";
-        } else {
-            contentType = "text/plain";
-        }
-        getResponse().setContentType(contentType);
-        getResponse().setHeader("Content-Type", contentType);
-
-        try {
-            OutputStream outs = getResponse().getOutputStream();
-            InputStream ins = new FileInputStream(new File(Configuration.i.getDataDirectory(),
-                "static/" + path));
-
-            byte[] buffer = new byte[1024];
-            int bytes;
-            while ((bytes = ins.read(buffer, 0, 1024)) > 0) {
-                outs.write(buffer, 0, bytes);
-            }
-
-            ins.close();
-        } catch (IOException e) {
-            log.error("Error sending static file!", e);
-        }
+    String contentType = "text/plain";
+    if (path.endsWith(".css")) {
+      contentType = "text/css";
+    } else if (path.endsWith(".js")) {
+      contentType = "text/javascript";
+    } else if (path.endsWith(".png")) {
+      contentType = "image/png";
+    } else if (path.endsWith(".ico")) {
+      contentType = "image/x-icon";
+    } else {
+      contentType = "text/plain";
     }
+    getResponse().setContentType(contentType);
+    getResponse().setHeader("Content-Type", contentType);
+
+    try {
+      OutputStream outs = getResponse().getOutputStream();
+      InputStream ins = new FileInputStream(new File(Configuration.i.getDataDirectory(),
+          "static/" + path));
+
+      byte[] buffer = new byte[1024];
+      int bytes;
+      while ((bytes = ins.read(buffer, 0, 1024)) > 0) {
+        outs.write(buffer, 0, bytes);
+      }
+
+      ins.close();
+    } catch (IOException e) {
+      throw new RequestException(404);
+    }
+  }
 }
