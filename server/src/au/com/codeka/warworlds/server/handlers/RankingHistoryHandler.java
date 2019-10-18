f664613a386131241375fcf8ac3b739c611c31ea
Dean Harding <dean@codeka.com.au>
2019-10-17 19:11:30 -0700

    Fix the display of ranking history.

--- server/src/au/com/codeka/warworlds/server/handlers/RankingHistoryHandler.java
+++ server/src/au/com/codeka/warworlds/server/handlers/RankingHistoryHandler.java
@@ -2,6 +2,7 @@ package au.com.codeka.warworlds.server.handlers;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+
 import java.math.BigInteger;
 
 import au.com.codeka.common.protobuf.Messages;
@@ -10,48 +11,47 @@ import au.com.codeka.warworlds.server.RequestHandler;
 import au.com.codeka.warworlds.server.data.DB;
 import au.com.codeka.warworlds.server.data.SqlResult;
 import au.com.codeka.warworlds.server.data.SqlStmt;
+import au.com.codeka.warworlds.server.model.GameHistory;
 
 public class RankingHistoryHandler extends RequestHandler {
-    @Override
-    protected void get() throws RequestException {
-        int year = Integer.parseInt(getUrlParameter("year"));
-        int month = Integer.parseInt(getUrlParameter("month"));
+  @Override
+  protected void get() throws RequestException {
+    int year = Integer.parseInt(getUrlParameter("year"));
+    int month = Integer.parseInt(getUrlParameter("month"));
 
-        String sql = "SELECT empire_id, rank, total_stars, total_colonies, total_buildings," +
-                           " total_ships, total_population" +
-                    " FROM empire_rank_histories"+
-                    " WHERE date >= ?" +
-                      " AND date < ?" +
-                    " ORDER BY rank, date DESC";
-        try (SqlStmt stmt = DB.prepare(sql)) {
-            stmt.setDateTime(1, new DateTime(year, month, 1, 0, 0, DateTimeZone.UTC));
-            stmt.setDateTime(2, new DateTime(year, month, 1, 0, 0, DateTimeZone.UTC).plusMonths(1));
-            SqlResult res = stmt.select();
+    String sql = "SELECT empire_id, rank, total_stars, total_colonies, total_buildings," +
+        " total_ships, total_population" +
+        " FROM empire_rank_histories" +
+        " WHERE game_history_id = (select max(id) from game_history where state = ?)" +
+        " ORDER BY rank";
+    try (SqlStmt stmt = DB.prepare(sql)) {
+      stmt.setInt(1, GameHistory.State.FINISHED.getValue());
+      SqlResult res = stmt.select();
 
-            int lastRank = 0;
-            Messages.EmpireRanks.Builder empire_ranks_pb = Messages.EmpireRanks.newBuilder();
-            empire_ranks_pb.setDate(new DateTime(year, month, 1, 0, 0, DateTimeZone.UTC).getMillis() / 1000);
-            while (res.next()) {
-                int rank = res.getInt(2);
-                if (rank == lastRank) {
-                    continue;
-                }
-                lastRank = rank;
-
-                Messages.EmpireRank.Builder empire_rank_pb = Messages.EmpireRank.newBuilder();
-                empire_rank_pb.setEmpireKey(Integer.toString(res.getInt(1)));
-                empire_rank_pb.setRank(rank);
-                empire_rank_pb.setTotalStars(res.getLong(3));
-                empire_rank_pb.setTotalColonies(res.getLong(4));
-                empire_rank_pb.setTotalBuildings(res.getLong(5));
-                empire_rank_pb.setTotalShips(res.getLong(6));
-                empire_rank_pb.setTotalPopulation(res.getLong(7));
-                empire_ranks_pb.addRanks(empire_rank_pb);
-            }
-
-            setResponseBody(empire_ranks_pb.build());
-        } catch(Exception e) {
-            throw new RequestException(e);
+      int lastRank = 0;
+      Messages.EmpireRanks.Builder empire_ranks_pb = Messages.EmpireRanks.newBuilder();
+      empire_ranks_pb.setDate(new DateTime(year, month, 1, 0, 0, DateTimeZone.UTC).getMillis() / 1000);
+      while (res.next()) {
+        int rank = res.getInt(2);
+        if (rank == lastRank) {
+          continue;
         }
+        lastRank = rank;
+
+        Messages.EmpireRank.Builder empire_rank_pb = Messages.EmpireRank.newBuilder();
+        empire_rank_pb.setEmpireKey(Integer.toString(res.getInt(1)));
+        empire_rank_pb.setRank(rank);
+        empire_rank_pb.setTotalStars(res.getLong(3));
+        empire_rank_pb.setTotalColonies(res.getLong(4));
+        empire_rank_pb.setTotalBuildings(res.getLong(5));
+        empire_rank_pb.setTotalShips(res.getLong(6));
+        empire_rank_pb.setTotalPopulation(res.getLong(7));
+        empire_ranks_pb.addRanks(empire_rank_pb);
+      }
+
+      setResponseBody(empire_ranks_pb.build());
+    } catch (Exception e) {
+      throw new RequestException(e);
     }
+  }
 }
