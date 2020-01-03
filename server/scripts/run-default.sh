96d07608d3a9a15f240dcad87a50e010b1ecaaab
Dean Harding <dean@codeka.com.au>
2020-01-02 22:12:49 -0800

    Updated the run-default script

--- server/scripts/run-default.sh
+++ server/scripts/run-default.sh
@@ -6,18 +6,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 function run_server()
 {
-  pushd $DIR
+  pushd $DIR > /dev/null
   SERVER_OPTS=""
   SERVER_OPTS="$SERVER_OPTS -Djava.awt.headless=true"
   SERVER_OPTS="$SERVER_OPTS -Dau.com.codeka.warworlds.server.ConfigFile=data/config-default.json"
   SERVER_OPTS="$SERVER_OPTS -Djava.util.logging.config.file=logging-default.properties"
-  SERVER_OPTS="$SERVER_OPTS" exec ./bin/server $*
-  popd
+  SERVER_OPTS="$SERVER_OPTS" ./bin/server $*
+  SERVER_STATUS=$?
+  popd > /dev/null
+  return $SERVER_STATUS
 }
 
 until run_server; do
   echo "War Worlds exited prematurely with exit code $?. Restarting.." >&2
   sleep 1
 done
-
+echo "War Worlds exited normally." >&2
 
