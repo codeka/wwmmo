e0f286ee5f1cf74d41b9c7d8a4a805ffbdb2e9f8
Dean Harding <dean@codeka.com.au>
2020-02-16 23:19:24 -0800

    Added deploy-server.bat for deploying from Windows.

---
+++ scripts/deploy-server.bat
@@ -0,0 +1,18 @@
+@ECHO OFF
+SET SCRIPTPATH=%~dp0
+
+rem no idea how to really do this...
+PUSHD %SCRIPTPATH%
+CD ..
+SET ROOTPATH=%CD%
+POPD
+
+SET DISTPATH=%ROOTPATH%\server\build\distributions\server.zip
+
+PUSHD %ROOTPATH%
+CALL gradlew.bat --daemon :server:distZip
+POPD
+
+rem copy the file from the build location to the server
+scp %DISTPATH% warworld@game.war-worlds.com:/home/warworld
+
