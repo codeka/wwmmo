8c72635956627cd2ce34537b7f8222a3b99b6902
Dean Harding <dean@codeka.com.au>
2020-07-21 23:23:42 -0700

    Fixed some lint warnings.

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -5,15 +5,16 @@ set -e
 SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 ROOTPATH=`dirname $SCRIPTPATH`
-APKPATH=$ROOTPATH/client/build/outputs/apk/client-release.apk
+APKPATH=$ROOTPATH/client/build/outputs/apk/release/client-release.apk
 
 pushd $ROOTPATH > /dev/null
 ./gradlew --daemon :client:assembleRelease $@
 popd > /dev/null
 
-# Assumes ANDROID_HOME is set to your Android SDK directory
-#BUILDTOOLS_DIR=`ls $ANDROID_HOME/build-tools/ | sort -V | tail -1`
-VERSIONCODE=`$ANDROID_HOME/build-tools/$BUILDTOOLS_DIR/aapt dump badging $APKPATH | egrep -o "versionCode='[0-9]+'" | egrep -o "[0-9]+"`
+# Assumes ANDROID_HOME is set to your Android SDK directory, if not we'll try this default
+BUILDTOOLS_DIR=`ls ${ANDROID_HOME:-$HOME/android/sdk}/build-tools/ | sort -V | tail -1`
+VERSIONCODE=`${ANDROID_HOME:-$HOME/android/sdk}/build-tools/$BUILDTOOLS_DIR/aapt \
+    dump badging $APKPATH | egrep -o "versionCode='[0-9]+'" | egrep -o "[0-9]+"`
 
 cp $APKPATH $ROOTPATH/../apk/warworlds-$VERSIONCODE.apk
 
