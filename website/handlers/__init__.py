f40d0eb4f1191f521530ce660f5d2369cfc53ac8
Dean Harding <dean@codeka.com.au>
2020-08-04 22:59:33 -0700

    Some changes to the website.

--- website/handlers/__init__.py
+++ website/handlers/__init__.py
@@ -14,7 +14,7 @@ import ctrl.tmpl
 
 # This value gets incremented every time we deploy so that we can cache bust
 # our static resources (css, js, etc)
-RESOURCE_VERSION = 89
+RESOURCE_VERSION = 91
 
 
 class BaseHandler(webapp.RequestHandler):
