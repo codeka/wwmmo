5746b86b3f282b9494b6c6bd65aafbaa65a418bf
Dean Harding <dean@codeka.com.au>
2020-12-08 23:18:20 -0800

    Converted OwnedPlanetActivity to a fragment.

--- client/src/main/java/au/com/codeka/warworlds/game/solarsystem/OwnedPlanetFragment.java
+++ client/src/main/java/au/com/codeka/warworlds/game/solarsystem/OwnedPlanetFragment.java
@@ -1,103 +1,79 @@
 package au.com.codeka.warworlds.game.solarsystem;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import au.com.codeka.BackgroundRunner;
-import au.com.codeka.common.Log;
-import au.com.codeka.common.model.BaseBuildRequest;
 import au.com.codeka.common.model.BaseColony;
-import au.com.codeka.common.protobuf.Messages;
-import au.com.codeka.warworlds.ActivityBackgroundGenerator;
 import au.com.codeka.warworlds.R;
-import au.com.codeka.warworlds.StyledDialog;
-import au.com.codeka.warworlds.api.ApiRequest;
-import au.com.codeka.warworlds.api.RequestManager;
-import au.com.codeka.warworlds.ctrl.PlanetDetailsView;
+import au.com.codeka.warworlds.ServerGreeter;
 import au.com.codeka.warworlds.eventbus.EventHandler;
-import au.com.codeka.warworlds.game.build.BuildActivity;
 import au.com.codeka.warworlds.model.Colony;
-import au.com.codeka.warworlds.model.Planet;
+import au.com.codeka.warworlds.model.EmpireManager;
+import au.com.codeka.warworlds.model.MyEmpire;
 import au.com.codeka.warworlds.model.Star;
 import au.com.codeka.warworlds.model.StarManager;
+import au.com.codeka.warworlds.ui.BaseFragment;
 
-public class OwnedPlanetFragment extends Fragment {
-  private static final Log log = new Log("OwnedPlanetActivity");
-
+/**
+ * A fragment that hosts {@link OwnedPlanetDetailsFragment}, and lets you swipe between all the
+ * "owned" planets on a star.
+ */
+public class OwnedPlanetFragment extends BaseFragment {
   private Star star;
-  private Planet planet;
-  private Colony colony;
-
-  private FocusView focusView;
-  private PlanetDetailsView planetDetails;
-  private TextView buildQueueLength;
+  private List<Colony> colonies;
+  private ViewPager viewPager;
+  private ColonyPagerAdapter colonyPagerAdapter;
 
   @Nullable
   @Override
   public View onCreateView(
-      @NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.planet_owned, container, false);
+      @NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    viewPager = new ViewPager(requireContext());
+    viewPager.setId(R.id.pager);
+    return viewPager;
   }
 
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
-    ActivityBackgroundGenerator.setBackground(view);
 
-    final Button buildButton = view.findViewById(R.id.build_btn);
-    buildButton.setOnClickListener(v -> {
-      if (star == null) {
-        return; // can happen before the star loads
+    colonyPagerAdapter = new ColonyPagerAdapter(getChildFragmentManager());
+    viewPager.setAdapter(colonyPagerAdapter);
+  }
+
+  public Star getStar() {
+    return star;
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    ServerGreeter.waitForHello(requireActivity(), (success, greeting) -> {
+      if (!success) {
+        // TODO: is this still a thing?
+      } else {
+        int starID = requireArguments().getInt("au.com.codeka.warworlds.StarID");
+        StarManager.eventBus.register(eventHandler);
+        Star star = StarManager.i.getStar(starID);
+        if (star != null) {
+          refresh(star);
+        }
       }
-      if (colony == null) {
-        return; // shouldn't happen, the button should be hidden.
-      }
-
-      Intent intent = new Intent(requireContext(), BuildActivity.class);
-      intent.putExtra("au.com.codeka.warworlds.StarKey", star.getKey());
-
-      Messages.Colony.Builder colony_pb = Messages.Colony.newBuilder();
-      colony.toProtocolBuffer(colony_pb);
-      intent.putExtra("au.com.codeka.warworlds.Colony", colony_pb.build().toByteArray());
-      startActivity(intent);
     });
-
-    planetDetails = view.findViewById(R.id.planet_details);
-    focusView = view.findViewById(R.id.focus);
-    view.findViewById(R.id.update_focus_btn).setOnClickListener(v -> focusView.save());
-    buildQueueLength = view.findViewById(R.id.build_queue_length);
-
-    view.findViewById(R.id.abandon_btn).setOnClickListener(v -> onAbandonColony());
-
-    if (requireContext() instanceof OwnedPlanetActivity) {
-      refresh(((OwnedPlanetActivity) requireContext()).getStar());
-    }
-  }
-
-  @Override
-  public void onStart() {
-    super.onStart();
-    StarManager.eventBus.register(eventHandler);
-  }
-
-  @Override
-  public void onStop() {
-    super.onStop();
-    StarManager.eventBus.unregister(eventHandler);
   }
 
   private final Object eventHandler = new Object() {
@@ -111,57 +87,74 @@ public class OwnedPlanetFragment extends Fragment {
     }
   };
 
-  private void onAbandonColony() {
-    new StyledDialog.Builder(requireContext()).setMessage(Html.fromHtml(
-        "Are you sure you want to abandon this colony? The colony will revert to native if you" +
-            " do."))
-        .setTitle("Abandon colony")
-        .setPositiveButton("Abandon", (dialog, which) -> {
-          new BackgroundRunner<Void>() {
-            @Override
-            protected Void doInBackground() {
-              String url = String.format("stars/%s/colonies/%s/abandon",
-                  colony.getStarKey(), colony.getKey());
+  private void refresh(Star star) {
+    boolean dataSetChanged = (this.star == null);
+    this.star = star;
 
-              RequestManager.i.sendRequest(new ApiRequest.Builder(url, "POST").build());
-              return null;
-            }
-
-            @Override
-            protected void onComplete(Void unused) {
-              // notify the StarManager that this star has been updated
-              StarManager.i.refreshStar(Integer.parseInt(colony.getStarKey()));
-
-              // also finish the activity and go back to the solarsystem view.
-              //TODO finish();
-            }
-          }.execute();
-
-          dialog.dismiss();;
-        })
-        .setNegativeButton("Cancel", null).create().show();
-  }
-
-  private void refresh(Star s) {
-    int planetIndex = requireArguments().getInt("au.com.codeka.warworlds.PlanetIndex");
-
-    star = s;
-    planet = (Planet) star.getPlanets()[planetIndex - 1];
-    for (BaseColony colony : star.getColonies()) {
-      if (colony.getPlanetIndex() == planetIndex) {
-        this.colony = (Colony) colony;
+    colonies = new ArrayList<>();
+    MyEmpire myEmpire = EmpireManager.i.getEmpire();
+    for (BaseColony c : star.getColonies()) {
+      if (c.getEmpireKey() != null && c.getEmpireKey().equals(myEmpire.getKey())) {
+        colonies.add((Colony) c);
       }
     }
+    Collections.sort(colonies, (lhs, rhs) -> lhs.getPlanetIndex() - rhs.getPlanetIndex());
+    colonyPagerAdapter.notifyDataSetChanged();
 
-    planetDetails.setup(star, planet, colony);
-    focusView.setColony(star, colony);
-
-    int totalBuildRequests = 0;
-    for (BaseBuildRequest buildRequest : star.getBuildRequests()) {
-      if (buildRequest.getPlanetIndex() == planetIndex) {
-        totalBuildRequests ++;
-      }
+    int initialPlanetIndex = requireArguments().getInt("au.com.codeka.warworlds.PlanetIndex", -1);
+    if (initialPlanetIndex < 0) {
+      initialPlanetIndex = colonies.get(0).getPlanetIndex();
     }
-    buildQueueLength.setText(String.format(Locale.US, "Build queue: %d", totalBuildRequests));
+
+    int colonyIndex = 0;
+    for (Colony colony : colonies) {
+      if (colony.getPlanetIndex() == initialPlanetIndex) {
+        break;
+      }
+      colonyIndex++;
+    }
+
+    if (dataSetChanged) {
+      viewPager.setCurrentItem(colonyIndex);
+    }
   }
+
+  public class ColonyPagerAdapter extends FragmentStatePagerAdapter {
+    public ColonyPagerAdapter(FragmentManager fm) {
+      super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+      Fragment fragment = new OwnedPlanetDetailsFragment();
+      Bundle args = new Bundle();
+      args.putInt("au.com.codeka.warworlds.StarID", star.getID());
+      args.putInt("au.com.codeka.warworlds.PlanetIndex", colonies.get(i).getPlanetIndex());
+      fragment.setArguments(args);
+      return fragment;
+    }
+
+    @Override
+    public int getCount() {
+      if (colonies == null) {
+        return 0;
+      }
+
+      return colonies.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return "Colony " + (position + 1);
+    }
+
+    @Override
+    public void setPrimaryItem(ViewGroup container, int position, Object object) {
+      super.setPrimaryItem(container, position, object);
+//      if (colonies != null && colonies.size() > position) {
+//        refreshColonyDetails(colonies.get(position));
+//      }
+    }
+  }
+
 }
