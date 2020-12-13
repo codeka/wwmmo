80be952384397a8d25b4d0c79a0732c1be53a09f
Dean Harding <dean@codeka.com.au>
2020-12-12 20:00:10 -0800

    Re-implemented a couple of features.

--- client/src/main/java/au/com/codeka/warworlds/ui/BaseFragment.java
+++ client/src/main/java/au/com/codeka/warworlds/ui/BaseFragment.java
@@ -1,5 +1,9 @@
 package au.com.codeka.warworlds.ui;
 
+import android.content.Context;
+import android.view.Display;
+import android.view.WindowManager;
+
 import androidx.fragment.app.Fragment;
 
 import javax.annotation.Nullable;
@@ -14,5 +18,17 @@ public class BaseFragment extends Fragment {
 
   protected MainActivity requireMainActivity() {
     return (MainActivity) requireContext();
+  }
+
+  /**
+   * Helper function to determine whether we're in portrait orientation or not.
+   */
+//  @SuppressWarnings("deprecation") // need to support older devices as well
+  protected boolean isPortrait() {
+    // TODO: use a resource selector instead.
+    Display display = ((WindowManager) requireContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+    int width = display.getWidth();
+    int height = display.getHeight();
+    return height > width;
   }
 }
