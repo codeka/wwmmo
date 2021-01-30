bc3799ca2ab0f938b9a1f103ef7db3d67febeccb
Dean Harding <dean@codeka.com.au>
2021-01-29 21:20:57 -0800

    Added a TODO to check the version of Java.The server can't run with files build with Java > 11.

--- scripts/deploy-server.py
+++ scripts/deploy-server.py
@@ -2,6 +2,8 @@
 import common
 import os
 
+# TODO: check that JDK is not > 11, which iwhat the server expects
+
 # Do the build
 os.system("cd " + str(common.rootPath) + " && " + common.gradleCmd + " --daemon :server:distZip")
 
